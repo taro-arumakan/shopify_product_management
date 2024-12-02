c4d894561c2119415d7845592da18dd59245fe26
taro-arumakan <taro.arumakan@gmail.com>
2024-12-02 12:27:12 +0900

    columns for each tag filled by True or False indicating the product has them tagged

--- tags_to_df.py
+++ tags_to_df.py
@@ -3,12 +3,15 @@ df = pd.read_csv('/Users/taro/Downloads/products_export_1-2.csv')
 df = df[df['Tags'].notnull()]
 df = df[['Handle', 'Title', 'Tags']]
 tagss = df['Tags'].to_list()
-tags = sum((t.split(', ') for t in tagss), [])
-tags = list(dict.fromkeys(tags))
+all_tags = sum((t.split(', ') for t in tagss), [])
+all_tags = list(dict.fromkeys(all_tags))
 
-tags_df = pd.DataFrame(columns=['Handle', 'Title'] + tags)
-for index, row in df.iterrows():
-    tagstf = [tag in row['Tags'].split(', ') for tag in tags]
-    tags_df.loc[index] = [row['Handle'], row['Title']] + tagstf
+def row_to_dict(row):
+    res = dict(Handle=row['Handle'],
+               Title=row['Title'])
+    res.update({tag: tag in row['Tags'].split(', ') for tag in all_tags})
+    return res
+rows = [row_to_dict(row) for i, row in df.iterrows()]
+tags_df = pd.DataFrame(rows, columns=['Handle', 'Title'] + all_tags)
 tags_df = tags_df.sort_values(by=['Handle', 'Title'])
 tags_df.to_csv('/Users/taro/Downloads/tags_df.csv', index=False)
