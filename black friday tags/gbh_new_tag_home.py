60ada381e02a94be74bdc27c8eead2dc31125e57
taro <taro.arumakan@gmail.com>
2024-12-22 19:16:44 +0900

    tagging

--- black friday tags/gbh_new_tag_home.py
+++ black friday tags/gbh_new_tag_home.py
@@ -1,27 +1,36 @@
+from functools import partial
 import pandas as pd
 
-products = pd.read_csv('/Users/taro/Downloads/gbh_products_export_20241120.csv')
+products = pd.read_csv('/Users/taro/Downloads/20241220_gbh_products.csv')
 products = products[products['Variant SKU'].notnull()]
 products['Variant SKU'] = products['Variant SKU'].apply(lambda x: x[1:] if x.startswith("'") else x)
 products = products.ffill()
 products = products[products['Status'] == 'active']
 products = products[['Handle', 'Title', 'Option1 Name', 'Option1 Value', 'Option2 Name', 'Option2 Value', 'Option3 Name', 'Option3 Value', 'Variant SKU', 'Tags']]
 skus = products['Variant SKU'].to_list()
-skus = [sku.replace("'", '') for sku in skus]
 
-df = pd.read_csv(r'/Users/taro/Downloads/GBH\ Japan\ EC_2024\ Black\ Friday\ -\ JP\ RRP\ \(판가\)\ HOME.csv'.replace('\\', ''), header=1)
-df = df[['CODE', 'ITEM', 'COLOR', 'BF 割引率']]
-df = df[(df['BF 割引率'].notnull()) & (df['BF 割引率']!='X')]
+df = pd.read_csv(r'/Users/taro/Downloads/20241218_gbh_holiday_sales_list_HOME.csv', header=1)
+df = df[['品番', 'ITEM', 'COLOR', 'SIZE', '年末年始\n割引率']]
+df = df[(df['年末年始\n割引率'].notnull()) & (df['年末年始\n割引率'].str.endswith('%'))].reset_index(drop=True)
+df['品番'] = df['品番'].str.strip()
 
-cond = (~df['CODE'].isin(skus)) & (df['CODE'].apply(lambda x: len(x)) == 4)
-df.loc[cond, 'CODE'] = df.loc[cond, 'CODE'].apply(lambda x: '0' + x)
+# cond = (~df['品番'].isin(skus)) & (df['品番'].apply(lambda x: len(x)) == 4)
+# df.loc[cond, '品番'] = df.loc[cond, '品番'].apply(lambda x: '0' + x)
 
-products = products[products['Variant SKU'].isin(df['CODE'].to_list())]
+print(f'missing SKU:\n{df[~df['品番'].isin(skus)]}')
 
-for percentage in dict.fromkeys(df['BF 割引率'].to_list()):
-    new_tag = f'2024 BF {percentage} OFF HOME'
-    skus = df[df['BF 割引率'] == percentage]['CODE'].to_list()
+products = products[products['Variant SKU'].isin(df['品番'].to_list())]
+
+def update_tags(tags, new_tag):
+    tags = tags.split(', ')
+    tags = [tag for tag in tags if not tag.startswith('2024 BF ')]
+    return ', '.join(tags + [new_tag])
+
+for percentage in dict.fromkeys(df['年末年始\n割引率'].to_list()):
+    new_tag = f'2024 Holiday {percentage} OFF HOME'
+    skus = df[df['年末年始\n割引率'] == percentage]['品番'].to_list()
     cond = products['Variant SKU'].isin(skus)
     print(new_tag, len(skus), len(products[cond]))
-    products.loc[cond, 'Tags'] = products.loc[cond, 'Tags'].apply(lambda tags: ', '.join([tags, new_tag]))
-products.to_csv('/Users/taro/Downloads/gbh_2024bf_new_tags_home.csv', index=False)
+    f = partial(update_tags, new_tag=new_tag)
+    products.loc[cond, 'Tags'] = products.loc[cond, 'Tags'].apply(f)
+products.to_csv('/Users/taro/Downloads/gbh_2024holiday_new_tags_home.csv', index=False)
