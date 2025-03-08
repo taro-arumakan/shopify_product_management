41b4df83f184db30840d8445c8aac66ae380e868
taro <taro.arumakan@gmail.com>
2025-03-08 16:46:28 +0900

    ignore files overridden by translated files

--- shopify_product_management/reassign_product_description_images.py
+++ shopify_product_management/reassign_product_description_images.py
@@ -16,7 +16,10 @@ GOOGLE_CREDENTIAL_PATH = os.getenv('GOOGLE_CREDENTIAL_PATH')
 localdir = '/Users/taro/Downloads/apricot_studios_psd_to_jpg'
 paths_by_product_title = {}
 for path in os.listdir(localdir):
-    paths_by_product_title.setdefault(path.split('_')[0], []).append(f'{localdir}/{path}')
+    if '_product_main' in path:
+        print(f'ignoring translated file: {path}')
+    else:
+        paths_by_product_title.setdefault(path.split('_')[0], []).append(f'{localdir}/{path}')
 
 
 other_localdir = '/Users/taro/Downloads/apricot_studios'
@@ -24,13 +27,20 @@ other_localdir = '/Users/taro/Downloads/apricot_studios'
 def sanitize_image_name(image_name):
     return image_name.replace(' ', '_').replace('[', '').replace(']', '_').replace('(', '').replace(')', '')
 
+def names_to_ignore(local_new_path):
+    parts = local_new_path.rsplit('/', 1)[-1].split('_')
+    seq = str(int(parts[-2]) - 1).zfill(2)
+    return ['_'.join(parts[:-2] + [seq] + parts[-1:])] + [local_new_path]
+
 for product_title, paths in paths_by_product_title.items():
   print(product_title)
+  ignore_names = sum([names_to_ignore(path) for path in paths], [])
+  other_paths = [f'{other_localdir}/{product_title}/{p}' for p in os.listdir(f'{other_localdir}/{product_title}')
+                 if '_product_detail_' in p and
+                    not p.endswith('.psd') and
+                    not p in ignore_names]
+  all_paths = sorted(paths + other_paths, key=lambda x: int(x.rsplit('/', 1)[-1].split('_')[-1].split('.')[0]))
   product_id = product_id_by_title(SHOPNAME, ACCESS_TOKEN, product_title)
   print(product_id)
-  other_paths = [f'{other_localdir}/{product_title}/{p}' for p in os.listdir(f'{other_localdir}/{product_title}')
-                 if '_product_detail_' in p and not p.endswith('.psd')]
-  all_paths = sorted(paths + other_paths, key=lambda x: x.rsplit('/', 1)[-1])
-  pprint.pprint([sanitize_image_name(p.rsplit('/', 1)[-1]) for p in all_paths])
-
+  pprint.pprint(all_paths, width=150)
   upload_and_assign_description_images_to_shopify(SHOPNAME, ACCESS_TOKEN, product_id, all_paths, DUMMY_PRODUCT)
