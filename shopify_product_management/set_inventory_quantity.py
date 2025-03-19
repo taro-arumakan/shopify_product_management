e3fa9ccc9894fd651fcab24c88e4ec75309e1be2
taro-arumakan <taro.arumakan@gmail.com>
2025-03-19 17:19:53 +0900

    set archive-epke inventory

--- shopify_product_management/set_inventory_quantity.py
+++ shopify_product_management/set_inventory_quantity.py
@@ -6,13 +6,13 @@ from google_utils import get_sheet_index_by_title, gspread_access
 from shopify_utils import location_id_by_name, set_inventory_quantity_by_sku_and_location_id
 
 load_dotenv(override=True)
-SHOPNAME = 'kumej'
+SHOPNAME = 'archive-epke'
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 print(ACCESS_TOKEN)
 GOOGLE_CREDENTIAL_PATH = os.getenv('GOOGLE_CREDENTIAL_PATH')
 
-GSPREAD_ID = '1buFubQ6Ng4JzYn4JjTsv8SQ2J1Qgv1yyVrs4yQUHfE0'
-SHEET_TITLE = '25ss'
+GSPREAD_ID = '18YPrX-1CqvAxmrE1P6jrtmewkKZtiInngQw2bOTacWg'
+SHEET_TITLE = '2025.3/13 Release'
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler()
@@ -25,22 +25,13 @@ def sku_quantity_map_from_sheet():
     worksheet = gspread_access(GOOGLE_CREDENTIAL_PATH).open_by_key(GSPREAD_ID).get_worksheet(sheet_index)
     rows = worksheet.get_all_values()
 
-    # start_row 1 base, columns are 0 base
-    status_index = 1
-    sku_index = 18
-    quantity_index = 19
+    # start_row 0 base, columns are 0 base
+    sku_index = 4
+    quantity_index = 12
 
-    start_row = None
-    for i, row in enumerate(rows):
-        if row[status_index].startswith('3/10'):
-            logger.info(f'starting at row {i}')
-            start_row = i
-            break
+    start_row = 3
     res = {}
     for row in rows[start_row:]:
-        if row[status_index] and not row[status_index].startswith('3/10'):
-            logger.info(f'breaking at row: {row}')
-            break
         assert row[sku_index] not in res, f'same sku found in multiple rows!!: {row[sku_index]}'
         res[row[sku_index]] = int(row[quantity_index])
     return res
@@ -52,7 +43,7 @@ def main():
     sku_quantity_map = sku_quantity_map_from_sheet()
     import pprint
     pprint.pprint(sku_quantity_map)
-    location_id = location_id_by_name(SHOPNAME, ACCESS_TOKEN, 'KUME warehouse')
+    location_id = location_id_by_name(SHOPNAME, ACCESS_TOKEN, 'Archivépke Warehouse')
     assert location_id, 'location id not found'
     for sku, quantity in sku_quantity_map.items():
         if sku in skip_skus:
