f7a7e8a9c1a00f951aaa6ff1f6cff3bd6116d892
taro <taro.arumakan@gmail.com>
2025-04-08 22:39:21 +0900

    set logging level

--- shopify_graphql_client/client.py
+++ shopify_graphql_client/client.py
@@ -10,6 +10,7 @@ from shopify_graphql_client.product_variants_to_products import ProductVariantsT
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
 
 class ShopifyGraphqlClient(InventoryManagement,
                            MediaManagement,
