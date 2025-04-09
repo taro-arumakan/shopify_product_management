ad979ccbe381c28ec3e80f2e9c7b8657fe356cef
taro-arumakan <taro.arumakan@gmail.com>
2025-04-09 15:00:34 +0900

    set  quantity as well

--- shopify_product_management/set_inventory_track.py
+++ shopify_product_management/set_inventory_track.py
@@ -19,12 +19,15 @@ def main():
     rows = get_rows(google_credential_path, sheet_id=sheet_id, sheet_name=sheet_name)
 
     location_names = ['Archivépke Warehouse', 'Envycube Warehouse']
+    aw_location_id = sgc.location_id_by_name(location_names[0])
     for row in rows[2:]:
         sku = row[string.ascii_lowercase.index('e')]
         print(sku)
         res = sgc.enable_and_activate_inventory(sku, location_names)
         pprint.pprint(res)
-
+        quantity = int(row[string.ascii_lowercase.index('m')])
+        res = sgc.set_inventory_quantity_by_sku_and_location_id(sku, aw_location_id, quantity)
+        pprint.pprint(res)
 
 if __name__ == '__main__':
     main()
