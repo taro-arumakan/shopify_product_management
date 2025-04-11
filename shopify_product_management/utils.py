fde51147814244336365810cde1dd81ca13dcc60
taro-arumakan <taro.arumakan@gmail.com>
2025-04-11 15:44:32 +0900

    credential in util

---
+++ shopify_product_management/utils.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from collections import namedtuple
+
+def credentials(shop_name):
+    assert(load_dotenv(override=True))
+    access_token = os.environ[f'{shop_name}-ACCESS_TOKEN']
+    google_credential_path = os.environ['GOOGLE_CREDENTIAL_PATH']
+    google_sheet_id = os.environ[f'{shop_name}-GSPREAD_ID']
+    res = {
+            'shop_name': shop_name,
+            'access_token': access_token,
+            'google_credential_path': google_credential_path,
+            'google_sheet_id': google_sheet_id
+        }
+    cred = namedtuple('Credentials', res.keys())
+    return cred(*res.values())
