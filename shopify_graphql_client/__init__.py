6678c246c86eec2f1888c4367e900725edab665d
taro <taro.arumakan@gmail.com>
2025-04-12 12:49:11 +0900

    get method for convenience

--- shopify_graphql_client/__init__.py
+++ shopify_graphql_client/__init__.py
@@ -1,1 +1,6 @@
 from shopify_graphql_client.client import ShopifyGraphqlClient
+
+def get(shop_name):
+    from shopify_product_management.utils import credentials
+    cred = credentials(shop_name)
+    return ShopifyGraphqlClient(cred.shop_name, cred.access_token)
