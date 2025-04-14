4ce62b73919b3ce3e2575866c8f66127880478a4
taro <taro.arumakan@gmail.com>
2025-04-14 22:06:21 +0900

    do not mandate a spreadsheet id

--- shopify_product_management/utils.py
+++ shopify_product_management/utils.py
@@ -6,7 +6,7 @@ def credentials(shop_name):
     assert(load_dotenv(override=True))
     access_token = os.environ[f'{shop_name}-ACCESS_TOKEN']
     google_credential_path = os.environ['GOOGLE_CREDENTIAL_PATH']
-    google_sheet_id = os.environ[f'{shop_name}-GSPREAD_ID']
+    google_sheet_id = os.environ.get(f'{shop_name}-GSPREAD_ID')
     res = {
             'shop_name': shop_name,
             'access_token': access_token,
