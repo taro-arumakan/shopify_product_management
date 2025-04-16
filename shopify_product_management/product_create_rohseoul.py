1c713b7eb4965ff2322ae6f238cd299a6ca77012
taro <taro.arumakan@gmail.com>
2025-04-16 11:44:19 +0900

    variants as product variants (not standalone products)

--- shopify_product_management/product_create_rohseoul.py
+++ shopify_product_management/product_create_rohseoul.py
@@ -6,7 +6,7 @@ from shopify_graphql_client.client import ShopifyGraphqlClient
 
 logging.basicConfig(level=logging.INFO)
 
-def product_info_lists_from_sheet(gai:GoogleApiInterface, sheet_id, sheet_name):
+def product_info_lists_from_sheet(gai:GoogleApiInterface, sheet_id, sheet_name, handle_suffix):
     start_row = 2
     column_product_attrs = dict(
         status=string.ascii_lowercase.index('a'),
@@ -18,13 +18,18 @@ def product_info_lists_from_sheet(gai:GoogleApiInterface, sheet_id, sheet_name):
         size_text=string.ascii_lowercase.index('u'),
         material=string.ascii_lowercase.index('v'),
         made_in=string.ascii_lowercase.index('w'),
+        )
+    column_variant_attrs = dict(
         sku=string.ascii_lowercase.index('f'),
         color=string.ascii_lowercase.index('j'),
         price=string.ascii_lowercase.index('m'),
         stock=string.ascii_lowercase.index('n'),
         drive_link=string.ascii_lowercase.index('p'),
-        )
-    return gai.to_products_list(sheet_id, sheet_name, start_row, column_product_attrs)
+    )
+    return gai.to_products_list(sheet_id, sheet_name, start_row, column_product_attrs,
+                                column_variant_attrs,
+                                handle_suffix=handle_suffix,
+                                row_filter_func=lambda row: row[column_product_attrs['status']].strip() == 'NEW')
 
 def get_size_table_html(size_text):
     lines = list(filter(None, map(str.strip, size_text.split('\n'))))
@@ -53,15 +58,15 @@ def create_a_product(sgc:ShopifyGraphqlClient, product_info, vendor, description
     logging.info(f'creating {product_info["title"]}')
     description_html = description_html_map[product_info['title']]
     tags = ','.join([product_info['release_date'], product_info['collection'], product_info['category']])
-    res = sgc.product_create_default_variant(handle=product_info['handle'],
-                                             title=product_info['title'],
-                                             description_html=description_html,
-                                             vendor=vendor, tags=tags,
-                                             price=product_info['price'],
-                                             sku=product_info['sku'])
-    res2 = sgc.update_variation_value_metafield(res['id'], product_info['color'])
-    res3 = sgc.enable_and_activate_inventory(product_info['sku'], [])
-    return (res, res2, res3)
+    options = [[{'カラー': color}, price, sku] for color, price, sku
+               in zip(product_info['color'], product_info['price'], product_info['sku'])]
+
+    res = sgc.product_create(title=product_info['title'],
+                             handle=product_info['handle'],
+                             description_html=description_html,
+                             vendor=vendor, tags=tags, option_lists=options)
+    res2 = [sgc.enable_and_activate_inventory(sku, []) for sku in product_info['sku']]
+    return (res, res2)
 
 def create_products(sgc:ShopifyGraphqlClient, product_info_list, vendor):
     description_html_map = {product_info['title']: get_description_html(sgc,
@@ -78,41 +83,59 @@ def create_products(sgc:ShopifyGraphqlClient, product_info_list, vendor):
 def update_stocks(sgc:ShopifyGraphqlClient, product_info_list):
     logging.info('updating inventory')
     location_id = sgc.location_id_by_name('Shop location')
-    return [sgc.set_inventory_quantity_by_sku_and_location_id(product_info['sku'],
-                                                              location_id,
-                                                              product_info['stock'])
-            for product_info in product_info_list]
+    sku_stock_map = {sku: stock for product_info in product_info_list for sku, stock in zip(product_info['sku'], product_info['stock'])}
+    return [sgc.set_inventory_quantity_by_sku_and_location_id(sku, location_id, stock)
+            for sku, stock in sku_stock_map.items()]
 
 def sort_key_func(k):
     def convert(text):
         return int(text) if text.isdigit() else text.lower()
     return ['0'] if k.split('.')[0] == 'b1' else [convert(c) for c in re.split('([0-9]+)', k)]
 
-def process_product_images(sgc:ShopifyGraphqlClient, gai:GoogleApiInterface, product_info):
-    product_id = sgc.product_id_by_sku(product_info['sku'])
-    drive_id = gai.drive_link_to_id(product_info['drive_link'])
-    local_paths = gai.drive_images_to_local(drive_id,
-                                            '/Users/taro/Downloads/rohseoul20250411/',
-                                            f'upload_20250411_{product_info['sku']}',
-                                            sort_key_func=sort_key_func)
-    return sgc.upload_and_assign_images_to_product(product_id, local_paths)
+def process_product_images(sgc:ShopifyGraphqlClient, gai:GoogleApiInterface, product_info, handle_suffix):
+    product_id = sgc.product_id_by_handle('-'.join(product_info['title'].lower().split(' ') + [handle_suffix]))
+    drive_ids = [gai.drive_link_to_id(drive_id) for drive_id in product_info['drive_link']]
+    skuss = [[sku] for sku in product_info['sku']]
+
+    local_paths = []
+    variant_image_positions = []
+
+    for drive_id, skus in zip(drive_ids, skuss):
+        variant_image_positions.append(len(local_paths))
+        local_paths += gai.drive_images_to_local(drive_id,
+                                                 '/Users/taro/Downloads/rohseoul20250411/',
+                                                 f'upload_20250411_{skus[0]}',
+                                                 sort_key_func=sort_key_func)
+    ress = []
+    ress.append(sgc.upload_and_assign_images_to_product(product_id, local_paths))
+    for skus, image_position in zip(skuss, variant_image_positions):
+        ress.append(sgc.assign_image_to_skus_by_position(product_id, image_position, skus))
+    return ress
+
 
 def main():
+    handle_suffix = '25ss-2nd'
     from utils import credentials
     import pprint
     cred = credentials('rohseoul')
     gai = GoogleApiInterface(cred.google_credential_path)
-    product_info_list = product_info_lists_from_sheet(gai, cred.google_sheet_id, '25SS 2차오픈(4월)(Summer 25)')
+    product_info_list = product_info_lists_from_sheet(gai, cred.google_sheet_id, '25SS 2차오픈(4월)(Summer 25)', handle_suffix)
+    len_list = len(product_info_list)
     product_info_list = [product_info for product_info in product_info_list if product_info['status'] == 'NEW']
+    assert len_list == len(product_info_list)
     sgc = ShopifyGraphqlClient(cred.shop_name, cred.access_token)
-    ress = create_products(sgc, product_info_list, cred.shop_name)
-    pprint.pprint(ress)
-    ress = update_stocks(sgc, product_info_list)
-    pprint.pprint(ress)
+    # ress = create_products(sgc, product_info_list, cred.shop_name)
+    # pprint.pprint(ress)
+    # ress = update_stocks(sgc, product_info_list)
+    # pprint.pprint(ress)
     ress = []
-    for product_info in product_info_list:
+    reprocess_from_title = 'Medium Mug shoulder bag Nylon'
+    for index, product_info in enumerate(product_info_list):
+        if product_info['title'] == reprocess_from_title:
+            break
+    for product_info in product_info_list[index:]:
         assert product_info['drive_link'], f"no drive link for {product_info['title']}"
-        ress.append(process_product_images(sgc, gai, product_info))
+        ress.append(process_product_images(sgc, gai, product_info, handle_suffix))
     pprint.pprint(ress)
 
 if __name__ == '__main__':
