a16489ca4de2ffe5e24f058702c3b93d91c18dec
taro <taro.arumakan@gmail.com>
2025-04-16 14:22:47 +0900

    refer the relocated module

--- shopify_graphql_client/__init__.py
+++ shopify_graphql_client/__init__.py
@@ -1,6 +1,6 @@
 from shopify_graphql_client.client import ShopifyGraphqlClient
 
 def get(shop_name):
-    from shopify_product_management.utils import credentials
+    from utils import credentials
     cred = credentials(shop_name)
     return ShopifyGraphqlClient(cred.shop_name, cred.access_token)
