7f13ddff4f21979caede539698822cee52fd7905
taro <taro.arumakan@gmail.com>
2025-04-16 13:10:47 +0900

    instance holds a sheet_id

--- google_api_interface/__init__.py
+++ google_api_interface/__init__.py
@@ -1,6 +1,6 @@
 from google_api_interface.interface import GoogleApiInterface
 
 def get(shop_name):
-    from shopify_product_management.utils import credentials
+    from utils import credentials
     cred = credentials(shop_name)
-    return GoogleApiInterface(cred.google_credential_path)
+    return GoogleApiInterface(cred.google_credential_path, sheet_id=cred.google_sheet_id)
--- google_api_interface/interface.py
+++ google_api_interface/interface.py
@@ -6,11 +6,12 @@ from google_api_interface.drive import GoogleDriveApiInterface
 from google_api_interface.sheets import GoogleSheetsApiInterface
 
 class GoogleApiInterface(GoogleDriveApiInterface, GoogleSheetsApiInterface):
-    def __init__(self, google_credential_path):
+    def __init__(self, google_credential_path, sheet_id=None):
         self.google_credential_path = google_credential_path
         self.scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
         self.credentials = Credentials.from_service_account_file(self.google_credential_path, scopes=self.scopes)
         self.drive_service = build('drive', 'v3', credentials=self.credentials)
         self.sheets_service = build('sheets', 'v4', credentials=self.credentials)
         self.gspread_client = gspread.authorize(self.credentials)
+        self.sheet_id = sheet_id
         self.logger = logging.getLogger(__name__)
