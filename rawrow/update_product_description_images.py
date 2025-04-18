3852135791fea79c9845e2b5839b07787bd89583
taro <taro.arumakan@gmail.com>
2025-04-18 16:50:45 +0900

    upadte missing description images. remap folder names to product names

--- rawrow/update_product_description_images.py
+++ rawrow/update_product_description_images.py
@@ -1,10 +1,9 @@
 
 import string
-import google_api_interface
-import shopify_graphql_client
 import utils
+import re
 
-IMAGES_LOCAL_DIR = '/Users/taro/Downloads/rawrowr20250321/'
+IMAGES_LOCAL_DIR = '/Users/taro/Downloads/rawrowr20250418/'
 DUMMY_PRODUCT = 'gid://shopify/Product/8773753700593'
 SHOPIFY_FILE_URL_PREFIX = 'https://cdn.shopify.com/s/files/1/0726/9187/6081/'
 
@@ -12,26 +11,37 @@ additional_punctuation_chars = '‘’“” '
 punctuation_chrs = string.punctuation + additional_punctuation_chars
 punctuation_translator = str.maketrans(punctuation_chrs, '_' * len(punctuation_chrs))
 
+def get_product_name(title):
+    if title.startswith('R TRUNK'):
+        res = title
+    else:
+        res = f'R BAG {title}'
+    res = res.replace('_', '/')
+    if r'" ' in res:
+        res = res[:res.find('" ')+1]
+    res = re.sub(r'\s*/\s*', ' / ', res)
+    # res = re.sub(r'\s+\d+$', '', res)
+    name_map = {'R BAG TRAVELOG CROSS': 'R BAG TRAVELOG CROSS 904'}
+    return name_map.get(res, res)
+
 def main():
-    cred = utils.credentials('rawrowr')
-    gai = google_api_interface.get(cred.shop_name)
-    folders = gai.list_folders(parent_folder_id='1ijec5ijfvFLvpzZGW7KmM7D0tQkUDky7')
-    sgc = shopify_graphql_client.get(cred.shop_name)
-    restart_from = "R TRUNK FRAME ep.3 88L _ 29'' 933"
-    for index, f in enumerate(folders):
-        if f['name'] == restart_from:
-            break
-    for f in folders[index:]:
+    client = utils.client('rawrowr')
+    folders = client.list_folders(parent_folder_id='1ijec5ijfvFLvpzZGW7KmM7D0tQkUDky7')
+    for f in folders:
         dir_id = f['id']
-        name = f['name']
-        if name != "R TRUNK FRAME ep.3 84L _ 20’’":
+        name = get_product_name(f['name'])
+        if name != "R TRUNK FRAME ep.3 84L / 20’’" and " COVER " not in name:
             prefix = name.translate(punctuation_translator)
-            local_paths = gai.drive_images_to_local(dir_id, IMAGES_LOCAL_DIR, download_filename_prefix=f'{prefix}_')
-            if local_paths:
-                product_id = sgc.product_id_by_title(name.replace('_', '/'))
-                print(f'upload {len(local_paths)} images to {product_id}')
-                sgc.upload_and_assign_description_images_to_shopify(product_id, local_paths, DUMMY_PRODUCT,
-                                                                    SHOPIFY_FILE_URL_PREFIX)
+            print('processing', name)
+            product = client.product_by_query(f"title:{name}*", additional_fields=['descriptionHtml'])
+            if not product['descriptionHtml']:
+                print(f'product {name} has no description, prrocessing')
+                local_paths = client.drive_images_to_local(dir_id, IMAGES_LOCAL_DIR, filename_prefix=f'{prefix}_')
+                if local_paths:
+                    product_id = client.product_id_by_title(name.replace('_', '/'))
+                    print(f'upload {len(local_paths)} images to {product_id}')
+                    client.upload_and_assign_description_images_to_shopify(product_id, local_paths, DUMMY_PRODUCT,
+                                                                        SHOPIFY_FILE_URL_PREFIX)
 
 if __name__ == '__main__':
     main()
