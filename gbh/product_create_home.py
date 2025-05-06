263f2535785d52c6bb671065a130f3836c945aa1
taro <taro.arumakan@gmail.com>
2025-05-06 15:31:32 +0900

    new products

--- gbh/product_create_home.py
+++ gbh/product_create_home.py
@@ -72,6 +72,29 @@ def product_info_list_from_sheet_size_options(gai:utils.Client, sheet_id, sheet_
     return gai.to_products_list(sheet_id, sheet_name, start_row, column_product_attrs,
                                 option1_attrs, row_filter_func=lambda row: row[string.ascii_lowercase.index('f')] in titles_with_size_options)
 
+def product_info_list_from_sheet_color_options(gai:utils.Client, sheet_id, sheet_name, titles_with_color_options):
+    start_row = 2
+    column_product_attrs = dict(
+        title=string.ascii_lowercase.index('f'),
+        category=string.ascii_lowercase.index('d'),
+        category2=string.ascii_lowercase.index('e'),
+        release_date=string.ascii_lowercase.index('b'),
+        description=string.ascii_lowercase.index('q'),
+        product_care=string.ascii_lowercase.index('s'),
+        material=string.ascii_lowercase.index('v'),
+        made_in=string.ascii_lowercase.index('w'),
+        size_text=string.ascii_lowercase.index('u'),
+        )
+    option1_attrs = {'カラー': string.ascii_lowercase.index('g')}
+    option1_attrs.update(
+        drive_link=string.ascii_lowercase.index('o'),
+        price=string.ascii_lowercase.index('l'),
+        sku=string.ascii_lowercase.index('i'),
+        stock=string.ascii_lowercase.index('m'),
+    )
+    return gai.to_products_list(sheet_id, sheet_name, start_row, column_product_attrs,
+                                option1_attrs, row_filter_func=lambda row: row[string.ascii_lowercase.index('f')] in titles_with_color_options)
+
 def create_a_product(sgc:utils.Client, product_info, vendor, size_texts=None, get_size_table_html_func=None):
     logging.info(f'creating {product_info["title"]}')
     description_html = sgc.get_description_html(description=product_info['description'],
@@ -103,28 +126,43 @@ def main():
     import pprint
     client = utils.client('gbhjapan')
     vendor = 'GBH'
-    titles_with_size_options = ['DRINKING GLASS / THIN']
+    # titles_with_size_options = ['DRINKING GLASS / THIN']
     # product_info_list = product_info_list_from_sheet_size_options(client, client.sheet_id, 'HOME&COSMETIC_25.04.24', titles_with_size_options)
     # size_texts = {option1['サイズ']: option1['size_text'] for option1 in product_info['options']}
     # ress = create_products(client, product_info_list, vendor, size_texts, size_table_html_from_size_dict)
     # for res in ress:
     #     logging.info(res)
 
-    titles_with_scent_and_size_options = ['FRAGRANCE SANITIZER SPRAY']
-    # product_info_list = product_info_list_from_sheet_scent_and_size_options(client, client.sheet_id, 'HOME&COSMETIC_25.04.24', titles_with_scent_and_size_options)
-    # ress = create_products(client, product_info_list, vendor, get_size_table_html_func=lambda x: x)
-
-    def filter_func(row):
-        return (isinstance(row[string.ascii_lowercase.index('b')], int) and
-                datetime.date(1899, 12, 30) + datetime.timedelta(days=row[string.ascii_lowercase.index('b')]) == datetime.date(2025, 4, 24) and
-                row[string.ascii_lowercase.index('f')] not in titles_with_size_options + titles_with_scent_and_size_options)
-    product_info_list = product_info_list_from_sheet_no_options(client, client.sheet_id, 'HOME&COSMETIC_25.04.24', raw_filter_func=filter_func)
-    ress = create_products(client, product_info_list, vendor, get_size_table_html_func=size_table_html)
+    titles_with_color_options = ['WOOD PLATE']
+    product_info_list = product_info_list_from_sheet_color_options(client, client.sheet_id, 'HOME&COSME_25.05.09', titles_with_color_options)
+    ress = create_products(client, product_info_list, vendor, get_size_table_html_func=lambda x: x)
     pprint.pprint(ress)
     ress = []
     for product_info in product_info_list:
-        ress.append(client.process_product_images(product_info, '/Users/taro/Downloads/gbh20250421/', 'upload_20250421_'))
+        ress.append(client.process_product_images(product_info, '/Users/taro/Downloads/gbh20250506/', 'upload_20250506_'))
     pprint.pprint(ress)
+
+    # titles_with_scent_and_size_options = ['HAND BALM']
+    # product_info_list = product_info_list_from_sheet_scent_and_size_options(client, client.sheet_id, 'HOME&COSME_25.05.09', titles_with_scent_and_size_options)
+    # ress = create_products(client, product_info_list, vendor, get_size_table_html_func=lambda x: x)
+    # pprint.pprint(ress)
+    # ress = []
+    # for product_info in product_info_list:
+    #     ress.append(client.process_product_images(product_info, '/Users/taro/Downloads/gbh20250506/', 'upload_20250506_'))
+    # pprint.pprint(ress)
+
+
+    # def filter_func(row):
+    #     return (isinstance(row[string.ascii_lowercase.index('b')], int) and
+    #             datetime.date(1899, 12, 30) + datetime.timedelta(days=row[string.ascii_lowercase.index('b')]) == datetime.date(2025, 4, 24) and
+    #             row[string.ascii_lowercase.index('f')] not in titles_with_size_options + titles_with_scent_and_size_options)
+    # product_info_list = product_info_list_from_sheet_no_options(client, client.sheet_id, 'HOME&COSMETIC_25.04.24', raw_filter_func=filter_func)
+    # ress = create_products(client, product_info_list, vendor, get_size_table_html_func=size_table_html)
+    # pprint.pprint(ress)
+    # ress = []
+    # for product_info in product_info_list:
+    #     ress.append(client.process_product_images(product_info, '/Users/taro/Downloads/gbh20250421/', 'upload_20250421_'))
+    # pprint.pprint(ress)
 
 if  __name__ == '__main__':
     main()
