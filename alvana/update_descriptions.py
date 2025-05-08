42b14daf63f1dd5fa8c49e3fb21070c3a572d851
taro <taro.arumakan@gmail.com>
2025-05-08 21:58:58 +0900

    update description

--- alvana/update_descriptions.py
+++ alvana/update_descriptions.py
@@ -2,44 +2,57 @@ import string
 import utils
 from alvana.size_text_to_html_table import size_text_to_html_table
 
+
 def get_description(product_description, material, made_in):
     description_html = product_description_template()
-    description_html = description_html.replace('${DESCRIPTION}', product_description)
-    description_html = description_html.replace('${MATERIAL}', material)
-    description_html = description_html.replace('${MADEIN}', made_in)
+    description_html = description_html.replace("${DESCRIPTION}", product_description)
+    description_html = description_html.replace("${MATERIAL}", material)
+    description_html = description_html.replace("${MADEIN}", made_in)
     return description_html
 
 
 def get_product_care(product_care_text):
     res = {
-        'type': 'root',
-        'children': [
-            {'children': [{'type': 'text', 'value': product_care_text.strip('\"').replace('●', '- ')}], 'type': 'paragraph'},
-        ]
+        "type": "root",
+        "children": [
+            {
+                "children": [
+                    {
+                        "type": "text",
+                        "value": product_care_text.strip('"').replace("●", "- "),
+                    }
+                ],
+                "type": "paragraph",
+            },
+        ],
     }
     return res
 
+
 def main():
-    client = utils.client('alvanas')
-    rows = client.worksheet_rows(client.sheet_id, 'Product Master')
+    client = utils.client("alvanas")
+    rows = client.worksheet_rows(client.sheet_id, "Product Master")
 
     for row in rows[1:]:
         title = row[1].strip()
         if not title:
-          continue
-        print(f'processing {title}')
+            continue
+        print(f"processing {title}")
         product_id = client.product_id_by_title(title)
-        size_table_html = size_text_to_html_table(row[string.ascii_uppercase.index('H')].strip())
-        res = client.update_size_table_html_metafield(product_id, size_table_html)
-        print(res)
-        product_description = row[string.ascii_uppercase.index('E')].strip()
-        made_in = row[string.ascii_uppercase.index('I')].strip()
-        material = row[string.ascii_uppercase.index('G')].strip()
-        res = client.update_product_description(product_id, get_description(product_description, material, made_in))
-        print(res)
-        res = client.update_product_care_metafield(product_id, get_product_care(row[string.ascii_uppercase.index('F')].strip()))
-        print(res)
-    print('done updating')
+        # size_table_html = size_text_to_html_table(row[string.ascii_uppercase.index('H')].strip())
+        # res = client.update_size_table_html_metafield(product_id, size_table_html)
+        # print(res)
+        product_description = row[string.ascii_uppercase.index("E")].strip()
+        if not product_description.startswith("#N/A"):
+            made_in = row[string.ascii_uppercase.index("I")].strip()
+            material = row[string.ascii_uppercase.index("G")].strip()
+            res = client.update_product_description(
+                product_id, get_description(product_description, material, made_in)
+            )
+            print(res)
+        # res = client.update_product_care_metafield(product_id, get_product_care(row[string.ascii_uppercase.index('F')].strip()))
+        # print(res)
+    print("done updating")
 
 
 def product_description_template():
@@ -64,5 +77,6 @@ def product_description_template():
 </body>
 </html>"""
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
