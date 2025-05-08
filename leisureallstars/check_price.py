bbca689ed2859827bd7e4a2993205f284f4f01f4
taro <taro.arumakan@gmail.com>
2025-05-09 08:13:47 +0900

    check price

--- leisureallstars/check_price.py
+++ leisureallstars/check_price.py
@@ -1,45 +1,65 @@
 import utils
-client = utils.client('what-youth-japan')
+
+client = utils.client("what-youth-japan")
 import pandas as pd
-df = pd.read_csv(r'/Users/taro/Documents/PMA\ Inc/leisure\ allstars/20240325_price_update_OF240225-EPOKHE-2024年3月よりの新上代記載\ /EPOKHEオーダーシート-Table\ 1.csv'.replace('\\', ''), header=10)
+
+df = pd.read_csv(
+    r"/Users/taro/Documents/PMA Inc/leisure allstars/SOH250508-EPOKHE-4.csv", header=10
+)
+
 
 def get_price(price):
     try:
-        return int(price.replace('¥', '').replace(',', ''))
+        return int(price.replace("¥", "").replace(",", ""))
     except Exception:
         pass
+
 
 skus_not_found = []
 unmatch = []
 match = []
+multiple_sksu = []
 
-for sku, price in zip(df['スタイルナンバー'], df['上代（税込）.1']):
+for sku, price in zip(df["スタイルナンバー"], df["上代（税込）"]):
     price = get_price(price)
     if price:
-        variant = client.variant_by_sku(sku)
-        if not variant:
-            skus_not_found.append(sku)
+        try:
+            variant = client.variant_by_sku(sku)
+        except RuntimeError as ex:
+            if "Multiple" in str(ex):
+                print(ex)
+                multiple_sksu.append((sku, price))
+            else:
+                skus_not_found.append(sku)
         else:
-            s_price = int(variant[0]['price'])
+            s_price = int(variant["price"])
             if price != s_price:
                 unmatch.append((sku, price, s_price))
             else:
                 match.append((sku, price))
 
 
-print('skus_not_found:')
+print("skus_not_found:")
 for sku in skus_not_found:
     print(sku)
 
 print()
-print('match:')
+print("match:")
 for sku, price in match:
-    print(f'{sku}\t{price}')
+    print(f"{sku}\t{price}")
 
 print()
-print('unmatch:')
+print("unmatch:")
 for sku, price, s_price in unmatch:
-    print(f'{sku}\t{price}\t{s_price}')
+    print(f"{sku}\t{price}\t{s_price}")
+
+print()
+print("multiple for a sku:")
+for sku, price in multiple_sksu:
+    print(f"{sku}\t{price}")
 
 df = pd.DataFrame.from_records(unmatch)
-df.to_csv(r'/Users/taro/Documents/PMA Inc/leisure allstars/20250508_price_unmatch.csv', index=False)
+df.to_csv(
+    r"/Users/taro/Documents/PMA Inc/leisure allstars/20250508_price_unmatch_2.csv",
+    index=False,
+)
