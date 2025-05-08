10400013d2388c64ec5a6f8063505da213ab2c1c
taro <taro.arumakan@gmail.com>
2025-05-09 08:12:47 +0900

    raise at variant_by_sku

--- helpers/shopify_graphql_client/product_queries.py
+++ helpers/shopify_graphql_client/product_queries.py
@@ -1,10 +1,13 @@
 import logging
 
+
 class ProductQueries:
     """
     A class to handle GraphQL queries related to products in Shopify, inherited by the ShopifyGraphqlClient class.
     """
+
     logger = logging.getLogger(f"{__module__}.{__qualname__}")
+
     def products_by_query(self, query_string, additional_fields=None):
         query = """
         query productsByQuery($query_string: String!) {
@@ -42,51 +45,60 @@ class ProductQueries:
                 }
             }
         }
-        """ % (f"\n{'\n'.join(additional_fields)}" if additional_fields else '')
-        variables = {
-            "query_string": query_string
-        }
+        """ % (
+            f"\n{'\n'.join(additional_fields)}" if additional_fields else ""
+        )
+        variables = {"query_string": query_string}
         res = self.run_query(query, variables)
-        res = res['products']['nodes']
+        res = res["products"]["nodes"]
         assert len(res) < 100, f"Too many products found for {query_string}: {len(res)}"
         return res
 
     def product_by_query(self, query_string, additional_fields=None):
         products = self.products_by_query(query_string, additional_fields)
         if len(products) != 1:
-            raise RuntimeError(f"{'Multiple' if products else 'No'} products found for {query_string}: {products}")
+            raise RuntimeError(
+                f"{'Multiple' if products else 'No'} products found for {query_string}: {products}"
+            )
         return products[0]
 
     def product_by_id(self, identifier, additional_fields=None):
-        return self.products_by_query(f"id:'{identifier.rsplit('/', 1)[-1]}'", additional_fields)[0]
+        return self.products_by_query(
+            f"id:'{identifier.rsplit('/', 1)[-1]}'", additional_fields
+        )[0]
 
     def products_by_title(self, title, additional_fields=None):
-        products = self.products_by_query(f"title:'{title.replace("'", "\\'")}'", additional_fields)
+        products = self.products_by_query(
+            f"title:'{title.replace("'", "\\'")}'", additional_fields
+        )
         if len(products) == 0:
             raise RuntimeError(f"No products found for {title}")
         return products
 
     def product_ids_by_title(self, title):
         res = self.products_by_title(title)
-        return [r['id'] for r in res]
+        return [r["id"] for r in res]
 
     def product_by_title(self, title, additional_fields=None):
-        return self.product_by_query(f"title:'{title.replace("'", "\\'")}'", additional_fields)
+        return self.product_by_query(
+            f"title:'{title.replace("'", "\\'")}'", additional_fields
+        )
 
     def product_id_by_title(self, title):
-        return self.product_by_title(title)['id']
+        return self.product_by_title(title)["id"]
 
     def product_by_handle(self, handle, additional_fields=None):
         return self.product_by_query(f"handle:'{handle}'", additional_fields)
 
     def product_id_by_handle(self, handle):
-        return self.product_by_handle(handle)['id']
+        return self.product_by_handle(handle)["id"]
 
     def products_by_tag(self, tag, additional_fields=None):
         return self.products_by_query(f"tag:'{tag}'", additional_fields)
 
     def product_variants_by_query(self, query):
-        query = """
+        query = (
+            """
         {
             productVariants(first:100, query: "%s") {
             nodes {
@@ -113,21 +125,24 @@ class ProductQueries:
             }
             }
         }
-        """ % query
+        """
+            % query
+        )
         res = self.run_query(query)
-        return res['productVariants']['nodes']
+        return res["productVariants"]["nodes"]
 
     def product_variants_by_product_id(self, product_id):
         product_id = self.sanitize_id(product_id)
-        product_id = product_id.rsplit('/', 1)[-1]
-        return self.product_variants_by_query(f'product_id:{product_id}')
+        product_id = product_id.rsplit("/", 1)[-1]
+        return self.product_variants_by_query(f"product_id:{product_id}")
 
     def product_variants_by_tag(self, tag):
         return self.product_variants_by_query(f"tag:'{tag}'")
 
     def product_id_by_variant_id(self, variant_id):
-        variant_id = self.sanitize_id(variant_id, 'ProductVariant')
-        query = """
+        variant_id = self.sanitize_id(variant_id, "ProductVariant")
+        query = (
+            """
         {
             productVariant(id:"%s") {
                 displayName,
@@ -137,21 +152,26 @@ class ProductQueries:
                 }
             }
         }
-        """ % variant_id
+        """
+            % variant_id
+        )
         res = self.run_query(query)
-        return res['productVariant']['product']['id']
+        return res["productVariant"]["product"]["id"]
 
     def product_id_by_sku(self, sku):
-        res = self.variant_by_sku(sku)
-        if len(res) != 1:
-            raise Exception(f"{'Multiple' if res['nodes'] else 'No'} variants found for {sku}: {res}")
-        return res[0]['product']['id']
+        variant_id = self.variant_id_by_sku(sku)
+        return self.product_id_by_variant_id(variant_id)
 
     def variant_by_sku(self, sku):
-        return self.product_variants_by_query(f'sku:{sku}')
+        if len(res := self.product_variants_by_query(f"sku:{sku}")) != 1:
+            raise RuntimeError(
+                f"{'Multiple' if res else 'No'} variants found for {sku}: {res}"
+            )
+        return res[0]
 
     def variant_by_variant_id(self, variant_id):
-        query = """
+        query = (
+            """
         {
             productVariant(id: "%s") {
                 id
@@ -164,13 +184,12 @@ class ProductQueries:
                 }
             }
         }
-        """ % variant_id
+        """
+            % variant_id
+        )
 
         res = self.run_query(query, {})
-        return res['productVariant']
+        return res["productVariant"]
 
     def variant_id_by_sku(self, sku):
-        res = self.variant_by_sku(sku)
-        if len(res) != 1:
-            raise Exception(f"{'Multiple' if res else 'No'} variants found for {sku}: {res}")
-        return res[0]['id']
+        return self.variant_by_sku(sku)["id"]
