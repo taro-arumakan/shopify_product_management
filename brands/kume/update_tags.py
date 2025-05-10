402eff4fecae8941a5c67ccb9b38b6cf9dcf04fc
taro <taro.arumakan@gmail.com>
2025-05-10 12:55:51 +0900

    update tags

--- brands/kume/update_tags.py
+++ brands/kume/update_tags.py
@@ -1,9 +1,8 @@
 import utils
 
 client = utils.client("kumej")
-products = client.products_by_query("tag:'new'")
-news = [product for product in products if "'new'" in product["tags"]]
+products = client.products_by_query("tag:'DRESSES'")
 
-for n in news:
-    new_tags = ["New Arrival" if tag == "'new'" else tag for tag in n["tags"]]
-    client.update_product_tags(n["id"], ",".join(new_tags))
+for p in products:
+    new_tags = ["DRESS" if tag == "DRESSES" else tag for tag in p["tags"]]
+    client.update_product_tags(p["id"], ",".join(new_tags))
