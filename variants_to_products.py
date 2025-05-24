3fa60d520a9adb58d24d6d012cea69c3d5c1716a
taro-arumakan <taro.arumakan@gmail.com>
2025-05-24 16:59:33 +0900

    processed a product

--- variants_to_products.py
+++ variants_to_products.py
@@ -5,18 +5,12 @@ import utils
 def main():
     logging.basicConfig(level=logging.INFO)
     titles = [
-        'Cropped Organza Collar Blouson Jacket',
-        'Fitted Button Knitted Vest',
-        'Organza Layered Midi Skirt',
-        'Fringe Long Sleeve Cardigan',
-        'Tweed Short Sleeve Blouse',
-        '[Women] Wrinkle Free Over-Fit Shirt',
-        'Pleated Ruffle Sleeveless Long Dress',
+        "flower Lace Mini Skirt",
     ]
-    client = utils.client('kumej')
+    client = utils.client("kumej")
     for product_title in titles:
         client.product_variants_to_products(product_title)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
