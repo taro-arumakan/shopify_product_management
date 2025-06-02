37f56409eff1fb89511df2f1db68d4a3c968b293
taro <taro.arumakan@gmail.com>
2025-06-03 07:38:11 +0900

    going back a few seasons for description update

--- brands/rohseoul/update_description.py
+++ brands/rohseoul/update_description.py
@@ -1,17 +1,49 @@
-from rohseoul.product_create import get_description_html, product_info_lists_from_sheet
+import string
 import utils
+from brands.rohseoul.product_create import get_description_html
+
+
+def product_info_lists_from_sheet(
+    gai: utils.Client, sheet_id, sheet_name, handle_suffix
+):
+    start_row = 2
+    column_product_attrs = dict(
+        title=string.ascii_lowercase.index("d"),
+        status=string.ascii_lowercase.index("a"),
+        release_date=string.ascii_lowercase.index("c"),
+        collection=string.ascii_lowercase.index("f"),
+        category=string.ascii_lowercase.index("g"),
+        description=string.ascii_lowercase.index("r"),
+        size_text=string.ascii_lowercase.index("u"),
+        material=string.ascii_lowercase.index("v"),
+        made_in=string.ascii_lowercase.index("w"),
+    )
+    column_variant_attrs = {"カラー": string.ascii_lowercase.index("i")}
+    column_variant_attrs.update(
+        sku=string.ascii_lowercase.index("e"),
+        price=string.ascii_lowercase.index("l"),
+        stock=string.ascii_lowercase.index("m"),
+        drive_link=string.ascii_lowercase.index("p"),
+    )
+    return gai.to_products_list(
+        sheet_id,
+        sheet_name,
+        start_row,
+        column_product_attrs,
+        column_variant_attrs,
+        handle_suffix=handle_suffix,
+    )
 
 
 def main():
     client = utils.client("rohseoul")
     product_info_list = product_info_lists_from_sheet(
-        client, client.sheet_id, "25SS 2차오픈(4월)(Summer 25)", "25ss-2nd"
+        client, client.sheet_id, "9/27 発売 24FW 新作", ""
     )
 
     for product_info in product_info_list:
         if product_info["title"] in [
-            "Pulpy crossbody bag Nylon",
-            "Pulpy crossbody bag Wrinkled",
+            "Pulpy crossbody bag Suede",
         ]:
             description_html = get_description_html(
                 client,
