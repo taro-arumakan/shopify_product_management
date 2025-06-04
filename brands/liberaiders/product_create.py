de14bf3c39d6a6803fa2668b379bde20b0f21203
taro <taro.arumakan@gmail.com>
2025-06-04 20:37:11 +0900

    onboarding liberaiders

---
+++ brands/liberaiders/product_create.py
@@ -0,0 +1,139 @@
+import logging
+import string
+import utils
+from brands.alvana.update_descriptions import get_description
+from brands.liberaiders.size_text_to_html_table import size_text_to_html_table
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def product_info_list_from_sheet(gai: utils.Client, sheet_id, sheet_name):
+    start_row = 1
+    column_product_attrs = dict(
+        title=string.ascii_lowercase.index("b"),
+        tags=string.ascii_lowercase.index("c"),
+        price=string.ascii_lowercase.index("d"),
+        description=string.ascii_lowercase.index("e"),
+        product_care=string.ascii_lowercase.index("f"),
+        material=string.ascii_lowercase.index("g"),
+        size_text_ja=string.ascii_lowercase.index("i"),
+        size_text_en=string.ascii_lowercase.index("j"),
+        made_in=string.ascii_lowercase.index("k"),
+        drive_link=string.ascii_lowercase.index("l"),
+    )
+    option1_attrs = {"カラー": string.ascii_lowercase.index("m")}
+    option2_attrs = {"サイズ": string.ascii_lowercase.index("n")}
+    option2_attrs.update(
+        sku=string.ascii_lowercase.index("o"),
+        stock=string.ascii_lowercase.index("p"),
+    )
+    return gai.to_products_list(
+        sheet_id,
+        sheet_name,
+        start_row,
+        column_product_attrs,
+        option1_attrs,
+        option2_attrs,
+    )
+
+
+def populate_option(product_info, option1_key, option2_key):
+    return [
+        [
+            {option1_key: option1[option1_key], option2_key: option2[option2_key]},
+            product_info["price"],
+            option2["sku"],
+        ]
+        for option1 in product_info["options"]
+        for option2 in option1["options"]
+    ]
+
+
+def create_a_product(sgc: utils.Client, product_info, vendor):
+    logging.info(f'creating {product_info["title"]}')
+    description_html = get_description(
+        product_info["description"], product_info["material"], product_info["made_in"]
+    )
+    tags = product_info["tags"]
+    options = populate_option(product_info, "カラー", "サイズ")
+    res = sgc.product_create(
+        title=product_info["title"],
+        description_html=description_html,
+        vendor=vendor,
+        tags=tags,
+        option_lists=options,
+    )
+    product_id = res["id"]
+    size_text_ja = product_info["size_text_ja"].strip()
+    if size_text_ja:
+        size_table_html_ja = size_text_to_html_table(size_text_ja)
+        res = sgc.update_size_table_html_ja_metafield(product_id, size_table_html_ja)
+        print(res)
+    size_text_en = product_info["size_text_en"].strip()
+    if size_text_en:
+        size_table_html_en = size_text_to_html_table(size_text_en)
+        res = sgc.update_size_table_html_en_metafield(product_id, size_table_html_en)
+        print(res)
+
+    # res2 = [
+    #     sgc.enable_and_activate_inventory(option2["sku"], [])
+    #     for option1 in product_info["options"]
+    #     for option2 in option1["options"]
+    # ]
+    # return (res, res2)
+
+
+def create_products(sgc: utils.Client, product_info_list, vendor):
+    ress = []
+    for product_info in product_info_list:
+        ress.append(create_a_product(sgc, product_info, vendor))
+    return ress
+
+
+def update_stocks(sgc: utils.Client, product_info_list):
+    logging.info("updating inventory")
+    location_id = sgc.location_id_by_name("Jingumae")
+    sku_stock_map = {
+        option2["sku"]: option2["stock"]
+        for product_info in product_info_list
+        for option1 in product_info["options"]
+        for option2 in option1["options"]
+        if option2.get("stock")
+    }
+    return [
+        sgc.set_inventory_quantity_by_sku_and_location_id(sku, location_id, stock)
+        for sku, stock in sku_stock_map.items()
+    ]
+
+
+def process_product_images(client: utils.Client, product_info):
+    product_id = client.product_id_by_title(product_info["title"])
+    local_paths = []
+    image_positions = []
+    drive_id = client.drive_link_to_id(product_info["drive_link"])
+    image_positions.append(len(local_paths))
+    local_paths += client.drive_images_to_local(
+        drive_id,
+        "/Users/taro/Downloads/liberaiders20250604/",
+        f"upload_20250604_{product_info['title']}",
+    )
+    return client.upload_and_assign_images_to_product(product_id, local_paths)
+
+
+def main():
+    from utils import client
+    import pprint
+
+    c = client("liberaiders")
+    product_info_list = product_info_list_from_sheet(c, c.sheet_id, "Product Master")
+    ress = create_products(c, product_info_list, vendor="liberaiders")
+    for product_info in product_info_list:
+        pprint.pprint(ress)
+        res = process_product_images(c, product_info)
+        pprint.pprint(res)
+    update_stocks(c, product_info_list)
+
+
+if __name__ == "__main__":
+    main()
---
+++ brands/liberaiders/update_descriptions.py
@@ -0,0 +1,90 @@
+import string
+import utils
+from brands.alvana.size_text_to_html_table import size_text_to_html_table
+
+
+def get_description(product_description, material, made_in):
+    description_html = product_description_template()
+    description_html = description_html.replace("${DESCRIPTION}", product_description)
+    description_html = description_html.replace("${MATERIAL}", material)
+    description_html = description_html.replace("${MADEIN}", made_in)
+    return description_html
+
+
+def get_product_care(product_care_text):
+    res = {
+        "type": "root",
+        "children": [
+            {
+                "children": [
+                    {
+                        "type": "text",
+                        "value": product_care_text.strip('"').replace("●", "- "),
+                    }
+                ],
+                "type": "paragraph",
+            },
+        ],
+    }
+    return res
+
+
+def main():
+    client = utils.client("alvanas")
+    rows = client.worksheet_rows(client.sheet_id, "Product Master")
+
+    for row in rows[1:]:
+        title = row[1].strip()
+        if not title:
+            continue
+        print(f"processing {title}")
+        product_id = client.product_id_by_title(title)
+        size_text_ja = row[string.ascii_uppercase.index("I")].strip()
+        if size_text_ja:
+            size_table_html_ja = size_text_to_html_table(size_text_ja)
+            res = client.update_size_table_html_ja_metafield(
+                product_id, size_table_html_ja
+            )
+            print(res)
+        size_text_en = row[string.ascii_uppercase.index("J")].strip()
+        if size_text_en:
+            size_table_html_en = size_text_to_html_table(size_text_en)
+            res = client.update_size_table_html_en_metafield(
+                product_id, size_table_html_en
+            )
+            print(res)
+        product_description = row[string.ascii_uppercase.index("E")].strip()
+        made_in = row[string.ascii_uppercase.index("I")].strip()
+        material = row[string.ascii_uppercase.index("G")].strip()
+        res = client.update_product_description(
+            product_id, get_description(product_description, material, made_in)
+        )
+        print(res)
+    print("done updating")
+
+
+def product_description_template():
+    return r"""<!DOCTYPE html>
+<html><body>
+  <div id="alvanaProduct">
+    <p>${DESCRIPTION}</p>
+    <br>
+    <table width="100%">
+      <tbody>
+        <tr>
+          <th>素材</th>
+          <td>${MATERIAL}</td>
+        </tr>
+        <tr>
+          <th>原産国</th>
+          <td>${MADEIN}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>"""
+
+
+if __name__ == "__main__":
+    main()
