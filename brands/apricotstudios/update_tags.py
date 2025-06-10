ed543c73a44fc020fa690c5656adda608112b066
taro-arumakan <taro.arumakan@gmail.com>
2025-06-10 18:57:37 +0900

    add a recognizable tag

--- brands/apricotstudios/update_tags.py
+++ brands/apricotstudios/update_tags.py
@@ -4,11 +4,9 @@ import utils
 
 def main():
     client = utils.client("apricot-studios")
-    products = client.products_by_collection_handle("25-summer-2nd")
+    products = client.products_by_tag("2025-06-11")
     for p in products:
-        new_tags = [
-            "25_summer_2nd" if t.upper() == "5/30(2ND)" else t for t in p["tags"]
-        ] + ["campaign_eligible"]
+        new_tags = p["tags"] + ["25 Summer Swim & Rain"]
         print(["title"], new_tags)
         pprint.pprint(client.update_product_tags(p["id"], ",".join(new_tags)))
 
