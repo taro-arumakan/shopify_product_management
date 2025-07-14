1df2051278e376b7af3b20dd0bf109ae4c7de791
taro-arumakan <taro.arumakan@gmail.com>
2025-07-14 13:15:07 +0900

    add a tag by SKU

--- brands/archivepke/add_tags.py
+++ brands/archivepke/add_tags.py
@@ -1,18 +1,38 @@
+import logging
 import utils
 
-client = utils.client("archive-epke")
-nylon_products = client.products_by_query(
-    "tag:'Nylon bag'", additional_fields=["status"]
-)
-nylon_products = [p for p in nylon_products if p["status"] == "ACTIVE"]
+logging.basicConfig(level=logging.INFO)
 
-took_products = client.products_by_query("title:*Took*", additional_fields=["status"])
-took_products = [
-    p
-    for p in took_products
-    if p["status"] == "ACTIVE" and p["title"] not in ["Took bag (Leather)"]
+skus = [
+    "OVBAX25032SWB",
+    "OVBAX25027PBK",
+    "OVBAX25027LKK",
+    "OVBAX25027SBL",
+    "OVBRX25005BLK",
+    "OVBRX25005COC",
+    "OVBAX25034SWB",
+    "OVBAX25034SWK",
+    "OVBAX25028BLK",
+    "OVBAX25028COC",
+    "OVBSX25106BEE",
+    "OVBSX25106GRM",
+    "OVBSX25105BEE",
+    "OVBSX25105GRM",
+    "OVBAX25107BLK",
+    "OVBAX25107BEE",
+    "OVBAX25107GRM",
+    "OVBTX25002BLK",
+    "OVBTX25002COC",
+    "OVBSX25103BEE",
+    "OVBSX25104BEE",
+    "OVBSX25007BLK",
+    "OVBSX25007LKK",
 ]
 
-for p in nylon_products + took_products:
-    new_tags = p["tags"] + ["2025_2Q_10%_Off"]
-    client.update_product_tags(p["id"], ",".join(new_tags))
+client = utils.client("archive-epke")
+for sku in skus:
+    product_id = client.product_id_by_sku(sku)
+    product = client.product_by_id(product_id)
+    tags = product["tags"]
+    tags += ["summer_season_sale"]
+    client.update_product_tags(product_id, ",".join(tags))
