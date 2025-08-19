327983554caec4cdf35cde56e581c85259263273
taro <taro.arumakan@gmail.com>
2025-08-19 20:52:16 +0900

    set discount by collection id

---
+++ brands/archivepke/set_discount.py
@@ -0,0 +1,23 @@
+import utils
+
+
+def main():
+    client = utils.client("archive-epke")
+    products = client.products_by_collection_id("444693577949")
+    for p in products:
+        for variant in p["variants"]["nodes"]:
+            compare_at_price = variant["compareAtPrice"] or variant["price"]
+            price = int(int(compare_at_price) * 0.9)
+            print(
+                f"Updating price of {variant['id']} from {compare_at_price} to {price}"
+            )
+            client.update_variant_attributes(
+                product_id=p["id"],
+                variant_id=variant["id"],
+                attribute_names=["price", "compareAtPrice"],
+                attribute_values=[str(price), str(compare_at_price)],
+            )
+
+
+if __name__ == "__main__":
+    main()
