888da566bdf838f364064c6263a57f2b46d2b369
taro <taro.arumakan@gmail.com>
2025-08-22 15:08:21 +0900

    activate and publish products through the client

--- brands/liberaiders/publish_products.py
+++ brands/liberaiders/publish_products.py
@@ -10,21 +10,9 @@ from brands.liberaiders.product_create import product_info_list_from_sheet
 
 def main():
     client = utils.client("liberaiders")
-    product_info_list = product_info_list_from_sheet(
-        client, client.sheet_id, "Product Master"
-    )
-    product_info_list = product_info_list[2:]
-    publications = client.publications()
-    for product_info in product_info_list:
-        product = client.product_by_title(product_info["title"])
-        if not product["media"]["nodes"]:
-            logging.info(f"not publishing {product_info['title']}")
-            continue
-        params = {"product_id": product["id"]}
-        for publication in publications:
-            params["publication_id"] = publication["id"]
-            client.publish_by_product_id_and_publication_id(**params)
-        client.update_product_status(product["id"], "ACTIVE")
+    products = client.products_by_query("status:'DRAFT'")
+    for product in products:
+        client.activate_and_publish_by_product_id(product["id"])
 
 
 if __name__ == "__main__":
