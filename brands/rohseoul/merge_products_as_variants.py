38a1492230bebab090ac0e9acf78b8f31f9b2045
taro <taro.arumakan@gmail.com>
2025-08-22 18:34:56 +0900

    process 18 products

--- brands/rohseoul/merge_products_as_variants.py
+++ brands/rohseoul/merge_products_as_variants.py
@@ -7,18 +7,24 @@ logging.basicConfig(level=logging.INFO)
 def main():
     client = utils.client("rohseoul")
     titles = [
-        "Curly tote bag",
-        "Goody bag",
-        "Large aline shoulder bag",
-        "Large mug shoulder bag nylon",
-        "Medium around hobo bag",
-        "Medium knot shoulder bag",
-        "Medium mug shoulder bag nylon",
-        "Medium pebble shoulder bag",
-        "Medium tin square tote bag",
-        "Mini around hobo bag",
-        "Mini mug tote bag",
-        "Mini pulpy hobo bag",
+        "Large mug shoulder bag",
+        "Medium mug shoulder bag",
+        "Mini pulpy hobo bag suede",
+        "Mini root backpack nylon",
+        "Mini tin square tote bag",
+        "Mini tin square tote bag suede",
+        "Oval button wallet",
+        "Pulpy crossbody bag",
+        "Pulpy crossbody bag nylon",
+        "Pulpy crossbody bag suede",
+        "Pulpy hobo bag",
+        "Pulpy hobo bag nylon",
+        "Ripple shoulder bag",
+        "Roh ball cap",
+        "Rohbit wappen t-shirt",
+        "Root backpack nylon",
+        "Taco shoulder bag",
+        "Tin square shoulder bag",
     ]
     for title in titles:
         client.merge_products_as_variants(title, "Shop location")
