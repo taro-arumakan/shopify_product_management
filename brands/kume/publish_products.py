0417615e7da7c3766666c6faec60429971b27e18
taro-arumakan <taro.arumakan@gmail.com>
2025-08-28 16:49:47 +0900

    publish products

---
+++ brands/kume/publish_products.py
@@ -0,0 +1,24 @@
+import datetime
+import pytz
+import logging
+import utils
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    scheduled_time = pytz.timezone("Asia/Tokyo").localize(
+        datetime.datetime(2025, 9, 1, 0, 0, 0)
+    )
+    client = utils.client("kumej")
+    products = client.products_by_tag("25FW")
+    for product in products:
+        logging.info(f"publishing product: {product['title']}")
+        client.activate_and_publish_by_product_id(
+            product["id"], scheduled_time=scheduled_time
+        )
+
+
+if __name__ == "__main__":
+    main()
