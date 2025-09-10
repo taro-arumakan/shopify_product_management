09b56fcf26d98482bcae741c6ccb4cd7b940cfed
taro <taro.arumakan@gmail.com>
2025-09-10 16:16:32 +0900

    2025-09-16

--- brands/rohseoul/publish_products.py
+++ brands/rohseoul/publish_products.py
@@ -9,10 +9,10 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     scheduled_time = pytz.timezone("Asia/Tokyo").localize(
-        datetime.datetime(2025, 8, 29, 0, 0, 0)
+        datetime.datetime(2025, 9, 16, 0, 0, 0)
     )
     client = utils.client("rohseoul")
-    products = client.products_by_tag("2025-08-29")
+    products = client.products_by_tag("2025-09-16")
     for product in products:
         client.activate_and_publish_by_product_id(
             product["id"], scheduled_time=scheduled_time
