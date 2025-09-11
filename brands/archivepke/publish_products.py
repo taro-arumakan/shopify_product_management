7c678db85e030875692c6126e33b5269598faa87
taro-arumakan <taro.arumakan@gmail.com>
2025-09-11 15:53:57 +0900

    publish on Sep 24th, 15% off.

--- brands/archivepke/publish_products.py
+++ brands/archivepke/publish_products.py
@@ -8,14 +8,18 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     scheduled_time = pytz.timezone("Asia/Tokyo").localize(
-        datetime.datetime(2025, 9, 18, 0, 0, 0)
+        datetime.datetime(2025, 9, 24, 0, 0, 0)
     )
     client = utils.client("archive-epke")
-    products = client.products_by_tag("2025-09-18")
+    products = client.products_by_tag("2025-09-26")
+    tags_map = {"2025-09-26": "2025-09-24"}
     for product in products:
         client.activate_and_publish_by_product_id(
             product["id"], scheduled_time=scheduled_time
         )
+
+        tags = [tags_map.get(t, t) for t in product["tags"]]
+        client.update_product_tags(product["id"], ",".join(tags))
 
 
 if __name__ == "__main__":
--- brands/archivepke/set_discount.py
+++ brands/archivepke/set_discount.py
@@ -3,13 +3,13 @@ import utils
 
 def main():
     client = utils.client("archive-epke")
-    products = client.products_by_collection_id("444693577949")
+    products = client.products_by_tag("2025-09-24")
     for p in products:
         for variant in p["variants"]["nodes"]:
             compare_at_price = variant["compareAtPrice"] or variant["price"]
-            price = int(int(compare_at_price) * 0.9)
+            price = int(int(compare_at_price) * 0.85)
             print(
-                f"Updating price of {variant['id']} from {compare_at_price} to {price}"
+                f"Updating price of {p['title']} - {variant['selectedOptions']} from {compare_at_price} to {price}"
             )
             client.update_variant_attributes(
                 product_id=p["id"],
