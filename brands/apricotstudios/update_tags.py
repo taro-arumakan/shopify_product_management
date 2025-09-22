254054821b8dd103bb4711a8366b949452a53c4e
taro <taro.arumakan@gmail.com>
2025-09-22 15:27:18 +0900

    redo tags

--- brands/apricotstudios/update_tags.py
+++ brands/apricotstudios/update_tags.py
@@ -1,17 +1,28 @@
 import logging
 import pprint
 import utils
+from product_create import product_info_list_from_sheet_color_and_size
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
     client = utils.client("apricot-studios")
-    products = client.products_by_tag("2025_sale_event")
-    for p in products:
-        new_tags = p["tags"] + ["Special Summer Sale"]
-        print(["title"], new_tags)
-        pprint.pprint(client.update_product_tags(p["id"], ",".join(new_tags)))
+    product_info_list = product_info_list_from_sheet_color_and_size(
+        client, client.sheet_id, "9.25 25Autumn(2nd)"
+    )
+    for product_info in product_info_list:
+        product = client.product_by_title(product_info["title"])
+        tags = ",".join(
+            [
+                product_info["collection"],
+                product_info["category"],
+                product_info["release_date"],
+            ]
+            + ["New Arrival", "25 Autumn", "25 Autumn 2nd"]
+        )
+        print(product_info["title"], tags)
+        pprint.pprint(client.update_product_tags(product["id"], tags))
 
 
 if __name__ == "__main__":
