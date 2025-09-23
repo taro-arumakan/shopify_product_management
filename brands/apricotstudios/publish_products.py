4a20aa69b9d54959aa48c148014ca2466e1d99a6
taro <taro.arumakan@gmail.com>
2025-09-23 22:15:04 +0900

    publishing 25AW 2nd

--- brands/apricotstudios/publish_products.py
+++ brands/apricotstudios/publish_products.py
@@ -8,10 +8,10 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     scheduled_time = pytz.timezone("Asia/Tokyo").localize(
-        datetime.datetime(2025, 9, 11, 0, 0, 0)
+        datetime.datetime(2025, 9, 25, 0, 0, 0)
     )
     client = utils.client("apricot-studios")
-    products = client.products_by_collection_id("456669462784")
+    products = client.products_by_collection_id("457808675072")
     for product in products:
         client.activate_and_publish_by_product_id(
             product["id"], scheduled_time=scheduled_time
