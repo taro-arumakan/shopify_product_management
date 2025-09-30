c25bf1aca13feb3407d9ae92c33ebd1bb06d0c6d
taro-arumakan <taro.arumakan@gmail.com>
2025-09-30 17:02:04 +0900

    add APRICOT to Daugher

--- brands/apricotstudios/update_tags.py
+++ brands/apricotstudios/update_tags.py
@@ -8,20 +8,10 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     client = utils.client("apricot-studios")
-    product_info_list = product_info_list_from_sheet_color_and_size(
-        client, client.sheet_id, "9.25 25Autumn(2nd)"
-    )
-    for product_info in product_info_list:
-        product = client.product_by_title(product_info["title"])
-        tags = ",".join(
-            [
-                product_info["collection"],
-                product_info["category"],
-                product_info["release_date"],
-            ]
-            + ["New Arrival", "25 Autumn", "25 Autumn 2nd"]
-        )
-        print(product_info["title"], tags)
+    products = client.products_by_tag("Daughter")
+    for product in products:
+        tags = ",".join(product["tags"] + ["APRICOT", "DAUGHTER"])
+        print(product["title"], tags)
         pprint.pprint(client.update_product_tags(product["id"], tags))
 
 
