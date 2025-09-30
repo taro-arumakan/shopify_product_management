8079be5a275a2c65ecf6da8e3c1bf0e02fcdc43d
taro-arumakan <taro.arumakan@gmail.com>
2025-09-30 17:34:09 +0900

    register shoes. custom size text parsing

---
+++ brands/blossom/product_create_shoes.py
@@ -0,0 +1,186 @@
+import datetime
+import logging
+import string
+import utils
+from brands.blossom.update_descriptions import get_description
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def product_info_list_from_sheet(
+    gai: utils.Client, sheet_id, sheet_name, row_filter_func=None
+):
+    start_row = 1  # 0 base
+    column_product_attrs = dict(
+        title=string.ascii_lowercase.index("b"),
+        tags=string.ascii_lowercase.index("c"),
+        price=string.ascii_lowercase.index("e"),
+        description=string.ascii_lowercase.index("g"),
+        product_care=string.ascii_lowercase.index("i"),
+        material=string.ascii_lowercase.index("j"),
+        size_text=string.ascii_lowercase.index("k"),
+        made_in=string.ascii_lowercase.index("l"),
+    )
+    option1_attrs = {"Color": string.ascii_lowercase.index("m")}
+    option1_attrs.update(
+        drive_link=string.ascii_lowercase.index("n"),
+    )
+    option2_attrs = {"Size": string.ascii_lowercase.index("o")}
+    option2_attrs.update(
+        sku=string.ascii_lowercase.index("p"),
+        stock=string.ascii_lowercase.index("q"),
+    )
+    return gai.to_products_list(
+        sheet_id,
+        sheet_name,
+        start_row,
+        column_product_attrs,
+        option1_attrs,
+        option2_attrs,
+        row_filter_func=row_filter_func,
+    )
+
+
+def populate_option(product_info, option1_key, option2_key):
+    return [
+        [
+            {option1_key: option1[option1_key], option2_key: option2[option2_key]},
+            product_info["price"],
+            option2["sku"],
+        ]
+        for option1 in product_info["options"]
+        for option2 in option1["options"]
+    ]
+
+
+def create_a_product(sgc: utils.Client, product_info, vendor):
+    logging.info(f'creating {product_info["title"]}')
+    description_html = get_description(
+        product_info.get("description", ""),
+        product_info.get("material", ""),
+        product_info.get("made_in", ""),
+    )
+    tags = product_info["tags"]
+    options = populate_option(product_info, "Color", "Size")
+    res = sgc.product_create(
+        title=product_info["title"],
+        description_html=description_html,
+        vendor=vendor,
+        tags=tags,
+        option_lists=options,
+    )
+    product_id = res["id"]
+    update_metafields(sgc, product_id, product_info)
+    res = [
+        sgc.enable_and_activate_inventory(option2["sku"], [])
+        for option1 in product_info["options"]
+        for option2 in option1["options"]
+    ]
+    print(res)
+
+
+def get_size_table_html(client, size_text):
+    pairs = map(str.strip, size_text.split("/"))
+    names, values = zip(*(pair.rsplit(" ", 1) for pair in pairs))
+    return client.generate_table_html(names, [values])
+
+
+def update_metafields(sgc: utils.Client, product_id, product_info):
+    size_text = product_info.get("size_text", "").strip()
+    size_table_html = get_size_table_html(sgc, size_text)
+    res = sgc.update_size_table_html_metafield(product_id, size_table_html)
+    print(res)
+    if product_care := product_info.get("product_care", "").strip():
+        res = sgc.update_product_care_metafield(
+            product_id, sgc.text_to_simple_richtext(product_care)
+        )
+        print(res)
+
+
+def create_products(sgc: utils.Client, product_info_list, vendor):
+    ress = []
+    for product_info in product_info_list:
+        ress.append(create_a_product(sgc, product_info, vendor))
+    return ress
+
+
+def update_stocks(sgc: utils.Client, product_info_list):
+    logging.info("updating inventory")
+    location_id = sgc.location_id_by_name("Blossom Warehouse")
+    sku_stock_map = {
+        option2["sku"]: option2["stock"]
+        for product_info in product_info_list
+        for option1 in product_info["options"]
+        for option2 in option1["options"]
+        if option2.get("stock")
+    }
+    return [
+        sgc.set_inventory_quantity_by_sku_and_location_id(sku, location_id, stock)
+        for sku, stock in sku_stock_map.items()
+    ]
+
+
+def process_product_images(client: utils.Client, product_info):
+    product_id = client.product_id_by_title(product_info["title"])
+    local_paths = []
+    image_positions = []
+    skuss = []
+    for variant in product_info["options"]:
+        if "drive_link" in variant:
+            # assert variant[
+            #     "drive_link"
+            # ], f"no drive link for {product_info['title'], {variant}}"
+            drive_id = client.drive_link_to_id(variant["drive_link"])
+            image_positions.append(len(local_paths))
+            skuss.append([v2["sku"] for v2 in variant["options"]])
+            local_paths += client.drive_images_to_local(
+                drive_id,
+                f"/Users/taro/Downloads/blossom{datetime.date.today():%Y%m%d}/",
+                f"upload_{datetime.date.today():%Y%m%d}_{variant['options'][0]['sku']}",
+            )
+    ress = []
+    ress.append(client.upload_and_assign_images_to_product(product_id, local_paths))
+    for image_position, skus in zip(image_positions, skuss):
+        ress.append(
+            client.assign_image_to_skus_by_position(product_id, image_position, skus)
+        )
+    return ress
+
+
+def check_size_texts(client: utils.Client, product_info_list):
+    for product_info in product_info_list:
+        size_text = product_info.get("size_text", "").strip()
+        if not size_text:
+            logging.warning(f'no size text for {product_info["title"]}')
+        else:
+            size_table_html = get_size_table_html(client, size_text)
+            print(size_table_html)
+
+
+def main():
+    c = utils.client("blossomhcompany")
+    product_info_list = product_info_list_from_sheet(c, c.sheet_id, "shoes")
+
+    # for index, product_info in enumerate(product_info_list):
+    #     if product_info["title"] == "Liberaiders PX LOGO TEE":
+    #         break
+    # product_info_list = product_info_list[index:]
+
+    check_size_texts(c, product_info_list=product_info_list)
+    import pprint
+
+    res = create_products(c, product_info_list, vendor="blossom")
+    pprint.pprint(res)
+    update_stocks(c, product_info_list)
+    for product_info in product_info_list:
+        res = process_product_images(c, product_info)
+        pprint.pprint(res)
+
+    for product_info in product_info_list:
+        product_id = c.product_id_by_title(product_info["title"])
+        c.activate_and_publish_by_product_id(product_id)
+
+
+if __name__ == "__main__":
+    main()
