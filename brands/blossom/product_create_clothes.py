833f2af2f69242ea21908797ba9d2ab65cbff479
taro-arumakan <taro.arumakan@gmail.com>
2025-10-01 14:37:29 +0900

    utilize client methods

--- brands/blossom/product_create_clothes.py
+++ brands/blossom/product_create_clothes.py
@@ -42,19 +42,7 @@ def product_info_list_from_sheet(
     )
 
 
-def populate_option(product_info, option1_key, option2_key):
-    return [
-        [
-            {option1_key: option1[option1_key], option2_key: option2[option2_key]},
-            product_info["price"],
-            option2["sku"],
-        ]
-        for option1 in product_info["options"]
-        for option2 in option1["options"]
-    ]
-
-
-def create_a_product(sgc: utils.Client, product_info, vendor):
+def create_a_product(sgc: utils.Client, product_info, vendor, locations):
     logging.info(f'creating {product_info["title"]}')
     description_html = get_description(
         product_info.get("description", ""),
@@ -62,21 +50,9 @@ def create_a_product(sgc: utils.Client, product_info, vendor):
         product_info.get("made_in", ""),
     )
     tags = product_info["tags"]
-    options = populate_option(product_info, "Color", "Size")
-    res = sgc.product_create(
-        title=product_info["title"],
-        description_html=description_html,
-        vendor=vendor,
-        tags=tags,
-        option_lists=options,
-    )
-    product_id = res["id"]
+    res = sgc.create_a_product(product_info, vendor, description_html, tags, locations)
+    product_id = res[0]["id"]
     update_metafields(sgc, product_id, product_info)
-    res = [
-        sgc.enable_and_activate_inventory(option2["sku"], [])
-        for option1 in product_info["options"]
-        for option2 in option1["options"]
-    ]
     print(res)
 
 
@@ -92,56 +68,6 @@ def update_metafields(sgc: utils.Client, product_id, product_info):
         print(res)
 
 
-def create_products(sgc: utils.Client, product_info_list, vendor):
-    ress = []
-    for product_info in product_info_list:
-        ress.append(create_a_product(sgc, product_info, vendor))
-    return ress
-
-
-def update_stocks(sgc: utils.Client, product_info_list):
-    logging.info("updating inventory")
-    location_id = sgc.location_id_by_name("Blossom Warehouse")
-    sku_stock_map = {
-        option2["sku"]: option2["stock"]
-        for product_info in product_info_list
-        for option1 in product_info["options"]
-        for option2 in option1["options"]
-        if option2.get("stock")
-    }
-    return [
-        sgc.set_inventory_quantity_by_sku_and_location_id(sku, location_id, stock)
-        for sku, stock in sku_stock_map.items()
-    ]
-
-
-def process_product_images(client: utils.Client, product_info):
-    product_id = client.product_id_by_title(product_info["title"])
-    local_paths = []
-    image_positions = []
-    skuss = []
-    for variant in product_info["options"]:
-        if "drive_link" in variant:
-            # assert variant[
-            #     "drive_link"
-            # ], f"no drive link for {product_info['title'], {variant}}"
-            drive_id = client.drive_link_to_id(variant["drive_link"])
-            image_positions.append(len(local_paths))
-            skuss.append([v2["sku"] for v2 in variant["options"]])
-            local_paths += client.drive_images_to_local(
-                drive_id,
-                f"/Users/taro/Downloads/blossom{datetime.date.today():%Y%m%d}/",
-                f"upload_{datetime.date.today():%Y%m%d}_{variant['options'][0]['sku']}",
-            )
-    ress = []
-    ress.append(client.upload_and_assign_images_to_product(product_id, local_paths))
-    for image_position, skus in zip(image_positions, skuss):
-        ress.append(
-            client.assign_image_to_skus_by_position(product_id, image_position, skus)
-        )
-    return ress
-
-
 def main():
     c = utils.client("blossomhcompany")
     product_info_list = product_info_list_from_sheet(c, c.sheet_id, "clothes")
@@ -150,20 +76,30 @@ def main():
     #     if product_info["title"] == "Liberaiders PX LOGO TEE":
     #         break
     # product_info_list = product_info_list[index:]
-
-    c.check_size_texts(
+    product_info_list = [
+        pi
+        for pi in product_info_list
+        if pi["title"] not in ["GENTO SHIRRING TOP", "GENTO BALLOON SKIRT"]
+    ]
+    c.sanity_check_product_info_list(
         product_info_list=product_info_list,
         text_to_html_func=c.formatted_size_text_to_html_table,
-        raise_on_error=False,
     )
 
     import pprint
 
-    res = create_products(c, product_info_list, vendor="blossom")
-    pprint.pprint(res)
-    update_stocks(c, product_info_list)
+    location = "Blossom Warehouse"
     for product_info in product_info_list:
-        res = process_product_images(c, product_info)
+        create_a_product(c, product_info, vendor="blossom", locations=[location])
+
+    c.update_stocks(product_info_list, location)
+
+    for product_info in product_info_list:
+        res = c.process_product_images(
+            product_info,
+            local_dir=f"/Users/taro/Downloads/blossom{datetime.date.today():%Y%m%d}/",
+            local_prefix=f"upload_{datetime.date.today():%Y%m%d}",
+        )
         pprint.pprint(res)
 
     for product_info in product_info_list:
