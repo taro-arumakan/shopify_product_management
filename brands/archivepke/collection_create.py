0cbc970327a3b710e665e889e3fce62f97f646eb
taro <taro.arumakan@gmail.com>
2025-10-18 00:14:19 +0900

    2025 POP-UP 15％OFF

--- brands/archivepke/collection_create.py
+++ brands/archivepke/collection_create.py
@@ -3,14 +3,30 @@ import utils
 
 logging.basicConfig(level=logging.INFO)
 
+skus = [
+    "OVBAX25002MCB",
+    "OVBAX25002BLK",
+    "OVBAX25001BLK",
+    "OVBAX25201GBG",
+    "OVBAX25201GCB",
+    "OVBAX25201GCM",
+    "OVBAX25213GBG",
+    "OVBAX25213GCB",
+    "OVBAX25212BLK",
+    "OVBAX25212SCM",
+    "OVBAX25211BLK",
+    "OVBAX25211MCB",
+    "OVBAX25228BLK",
+    "OVBAX25228OLG",
+    "OVBTX25202BLK",
+    "OVBTX25202DKB",
+]
+
 
 def main():
     client = utils.client("archive-epke")
-    products = client.products_by_collection_handle("took-bag")
-    products += client.products_by_collection_handle("standard-line")
-
-    product_ids = [product["id"] for product in products]
-    client.collection_create("Sunny Essentials Sale", product_ids)
+    product_ids = {client.product_id_by_sku(sku) for sku in skus}
+    client.collection_create("2025 POP-UP 15% OFF", product_ids)
 
 
 if __name__ == "__main__":
