6c130641fb71ac8fa171e4016e2eb926141c121a
taro-arumakan <taro.arumakan@gmail.com>
2025-10-22 09:03:41 +0900

    add a tag. publish.

--- brands/rohseoul/publish_products.py
+++ brands/rohseoul/publish_products.py
@@ -9,10 +9,10 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     scheduled_time = datetime.datetime(
-        2025, 9, 16, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2025, 10, 24, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
     client = utils.client("rohseoul")
-    products = client.products_by_tag("2025-09-16")
+    products = client.products_by_tag("2025-10-24")
     for product in products:
         client.activate_and_publish_by_product_id(
             product["id"], scheduled_time=scheduled_time
