adc28cf4594c0a31bf9af77969b3df3730c681cc
taro-arumakan <taro.arumakan@gmail.com>
2025-10-24 08:53:11 +0900

    use a method in the base

--- brands/ssil/product_create.py
+++ brands/ssil/product_create.py
@@ -13,16 +13,13 @@ def main():
     # for i, product_info in enumerate(product_info_list):
     #     if product_info["title"] == "Lucky Clover Drop R_WG":
     #         break
-    product_info_list = product_info_list[i:]
+    # product_info_list = product_info_list[i:]
     c.sanity_check_product_info_list(product_info_list)
-    product_ids = []
     for product_info in product_info_list:
-        product_ids.append(c.create_a_product(product_info))
+        c.create_a_product(product_info)
         c.process_product_images(product_info)
     c.update_stocks(product_info_list)
-    for pi in product_info_list:
-        product_id = c.product_id_by_title(pi["title"])
-        c.activate_and_publish_by_product_id(product_id)
+    c.publish_products(product_info_list)
 
 
 if __name__ == "__main__":
