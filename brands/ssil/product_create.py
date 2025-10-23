8bec5595b7045f01bcbf43e8b2291bbbdbe2efce
taro-arumakan <taro.arumakan@gmail.com>
2025-10-23 17:56:52 +0900

    process all products

--- brands/ssil/product_create.py
+++ brands/ssil/product_create.py
@@ -10,17 +10,18 @@ def main():
     product_info_list = c.product_info_list_from_sheet(
         "material & size options (rings etc)"
     )
-    for i, product_info in enumerate(product_info_list):
-        if product_info["title"] == "Lucky Clover Drop R_WG":
-            break
+    # for i, product_info in enumerate(product_info_list):
+    #     if product_info["title"] == "Lucky Clover Drop R_WG":
+    #         break
     product_info_list = product_info_list[i:]
     c.sanity_check_product_info_list(product_info_list)
     product_ids = []
     for product_info in product_info_list:
         product_ids.append(c.create_a_product(product_info))
         c.process_product_images(product_info)
-    # c.update_stocks(product_info_list)
-    for product_id in product_ids:
+    c.update_stocks(product_info_list)
+    for pi in product_info_list:
+        product_id = c.product_id_by_title(pi["title"])
         c.activate_and_publish_by_product_id(product_id)
 
 
