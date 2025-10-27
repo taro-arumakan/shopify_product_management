8cf8659fb1af71959cd4583af7ebf6dff8fc058e
taro <taro.arumakan@gmail.com>
2025-10-27 14:25:25 +0900

    enable tracking - must be a bug in merging products as variants

---
+++ brands/archivepke/enable_tracking.py
@@ -0,0 +1,35 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+import utils
+
+
+titles = [
+    "Egg wallet bag",
+    "Fling bag",
+    "Freckle fold bag",
+    "Luv basket bag",
+    "Luv lock duffle bag",
+    "Luv moon bag",
+    "Macaroon bag",
+    "Ridge shoulder bag",
+    "Clip rucksack",
+    "Small fling bag",
+    "Took bag (Leather)",
+]
+location_names = ["Archivépke Warehouse", "Envycube Warehouse"]
+
+
+def main():
+    client = utils.client("archive-epke")
+    for title in titles:
+        products = client.products_by_query(f"title:'{title}' AND status:'ACTIVE'")
+        for product in products:
+            for variant in product["variants"]["nodes"]:
+                logging.info(f'activating {variant["sku"]} at {location_names}')
+                client.enable_and_activate_inventory(variant["sku"], location_names)
+
+
+if __name__ == "__main__":
+    main()
