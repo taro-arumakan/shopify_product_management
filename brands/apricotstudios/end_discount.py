3d45f3c8a3d0668edc3cd80ab31a65ae79c9af66
taro <taro.arumakan@gmail.com>
2025-10-27 14:25:45 +0900

    end discounts - Flow job has failed

---
+++ brands/apricotstudios/end_discount.py
@@ -0,0 +1,26 @@
+import utils
+
+
+def main():
+    client = utils.client("apricot-studios")
+    products = client.products_by_query(
+        'metafields.custom.discount_rate:"15%" AND status:ACTIVE'
+    )
+    for p in products:
+        for variant in p["variants"]["nodes"]:
+            price = int(variant["price"])
+            compare_at_price = int(variant["compareAtPrice"])
+            assert compare_at_price
+            print(
+                f"Updating price of {p['title']} - {variant['selectedOptions']} from {price} to {compare_at_price}"
+            )
+            client.update_variant_attributes(
+                product_id=p["id"],
+                variant_id=variant["id"],
+                attribute_names=["compareAtPrice"],
+                attribute_values=[str(compare_at_price)],
+            )
+
+
+if __name__ == "__main__":
+    main()
