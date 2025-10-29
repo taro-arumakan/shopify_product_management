9d06c31d2e9f9b465a5de3e9223f9a58b4000f4a
taro-arumakan <taro.arumakan@gmail.com>
2025-10-29 17:08:27 +0900

    through client

--- brands/gbh/add_variant.py
+++ brands/gbh/add_variant.py
@@ -1,53 +1,15 @@
-import datetime
 import logging
-import pathlib
 import utils
-from brands.gbh.product_create_apparel import (
-    product_info_list_from_sheet_color_and_size,
-)
 
 logging.basicConfig(level=logging.INFO)
 
-client = utils.client("gbhjapan")
-processing_skus = ["APB3CD070NYFF"]
-product_title_map = {"MERINO WOOL HIGHNECK CARDIGAN": "MERINOWOOL HIGHNECK CARDIGAN"}
 
-product_info_list = product_info_list_from_sheet_color_and_size(
-    client, client.sheet_id, "APPAREL 25FW (FALL 1次)"
-)
-for product_info in product_info_list:
-    for option1 in product_info["options"]:
-        for option2 in option1["options"]:
-            if option2["sku"] in processing_skus:
-                logging.info(
-                    f"going to process: {option2['sku']} - {product_info['title']}"
-                )
-                product_id = client.product_id_by_title(
-                    product_title_map.get(product_info["title"], product_info["title"])
-                )
-                drive_links, skuss = client.populate_drive_ids_and_skuss(product_info)
-                for drive_link, skus in zip(drive_links, skuss):
-                    if any(ps in skus for ps in processing_skus):
-                        logging.info(f"  processing sku: {skus} - {drive_link}")
-                        res = client.add_product_images(
-                            product_id,
-                            drive_link,
-                            f"{pathlib.Path.home()}/Downloads/gbh{datetime.date.today():%Y%m%d}/",
-                            f"upload_{datetime.date.today():%Y%m%d}_{skuss[0][0]}_",
-                        )
+def main():
+    client = utils.client("gbhjapan")
+    product_info_list = client.product_info_list_from_sheet("APPAREL 25FW (WINTER 1次)")
+    product_info = product_info_list[0]
+    client.add_variants_from_product_info(product_info)
 
-                        new_media_ids = [
-                            m["id"] for m in res[-1]["productCreateMedia"]["media"]
-                        ]
 
-                        client.variants_add(
-                            product_id,
-                            skus,
-                            [],
-                            [new_media_ids[0]],
-                            ["カラー", "サイズ"],
-                            [[option1["カラー"], "FREE"]],
-                            [option2["price"]],
-                            [option2["stock"]],
-                            client.location_id_by_name("Shop location"),
-                        )
+if __name__ == "__main__":
+    main()
--- brands/gbh/update_images_by_variant.py
+++ brands/gbh/update_images_by_variant.py
@@ -2,42 +2,26 @@ import datetime
 import logging
 import pathlib
 import utils
-from brands.gbh.product_create_apparel import (
-    product_info_list_from_sheet_color_and_size,
-)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-UPLOAD_IMAGE_PREFIX = f"uplaod{datetime.date.today():%Y%m%d}_gbh"
-IMAGES_LOCAL_DIR = f"{pathlib.Path.home()}/Downloads/{datetime.date.today():%Y%m%d}/"
-
 
 def main():
     client = utils.client("gbhjapan")
-    product_info_list = product_info_list_from_sheet_color_and_size(
-        client, client.sheet_id, "APPAREL 25FW (FALL 1次)"
+    product_info_list = client.product_info_list_from_sheet(
+        "APPAREL 25FW (WINTER 1次 画像変更)"
     )
-    for index, pi in enumerate(product_info_list):
-        if pi["title"] == "SKIRT WRAP BELT":
-            break
-    product_info_list = product_info_list[index:]
-
-    for product_info in product_info_list:
-        for option1 in product_info["options"]:
-            drive_link = option1["drive_link"]
-            drive_id = client.drive_link_to_id(drive_link)
-            skus = [option2["sku"] for option2 in option1["options"]]
-            if "APB3KN040SBFF" not in skus:
-                logger.info(
-                    f"going to process: {product_info['title']} - {skus} - {drive_id}"
-                )
-                client.replace_images_by_skus(
-                    skus,
-                    drive_id,
-                    IMAGES_LOCAL_DIR,
-                    download_filename_prefix=f"{UPLOAD_IMAGE_PREFIX}_{skus[0]}_",
-                )
+    product_info = product_info_list[0]
+    for option1 in product_info["options"]:
+        drive_link = option1["drive_link"]
+        drive_id = client.drive_link_to_id(drive_link)
+        skus = [option2["sku"] for option2 in option1["options"]]
+        logger.info(f"going to process: {product_info['title']} - {skus} - {drive_id}")
+        client.replace_images_by_skus(
+            skus,
+            drive_id,
+        )
 
 
 if __name__ == "__main__":
