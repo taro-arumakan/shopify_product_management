07752fb586770542a7394d090a77cfa3bc674b54
taro-arumakan <taro.arumakan@gmail.com>
2025-10-31 15:33:51 +0900

    use base class method

--- brands/rohseoul/product_create.py
+++ brands/rohseoul/product_create.py
@@ -8,15 +8,13 @@ def main():
     handle_suffix = "25fw-3rd"
 
     client = RohseoulClient()
-    product_info_list = client.product_info_lists_from_sheet(
-        sheet_name="25FW WINTER - copy", handle_suffix=handle_suffix
+    sheet_name = "25FW WINTER - copy"
+    client.sanity_check_sheet(sheet_name, handle_suffix=handle_suffix)
+    client.process_sheet_to_products(
+        sheet_name=sheet_name,
+        handle_suffix=handle_suffix,
+        additional_tags=["25_winter", "New Arrival"],
     )
-    client.sanity_check_product_info_list(product_info_list)
-    for product_info in product_info_list:
-        client.create_product_from_product_info(product_info)
-        client.process_product_images(product_info, handle_suffi=handle_suffix)
-    client.update_stocks(product_info_list)
-    client.publish_products(product_info_list)
 
 
 if __name__ == "__main__":
