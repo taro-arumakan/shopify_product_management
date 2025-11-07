28614efe77a0921dfd3c55d8a602ec903df8fce4
taro-arumakan <taro.arumakan@gmail.com>
2025-11-07 14:46:00 +0900

    archive products

--- brands/gbh/archive_products.py
+++ brands/gbh/archive_products.py
@@ -5,19 +5,12 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    client = utils.client("gbhjapan")
-    rows = client.worksheet_rows(client.sheet_id, "APPAREL 24SS_削除希望")
-
-    for row in rows[4:]:
-        try:
-            product_id = client.product_id_by_sku(row[8])
-            client.update_product_status(product_id, "ARCHIVED")
-            print(f"Archived {row[8]} - {row[5]}")
-        except (
-            utils.NoVariantsFoundException,
-            utils.MultipleVariantsFoundException,
-        ) as ex:
-            print(f"Error finding {row[8]} - {row[5]}, {ex}")
+    client = utils.client("gbh")
+    for tag in ["24fall", "24 winter"]:
+        products = client.products_by_tag(tag)
+        products = [p for p in products if not any("25" in t for t in p["tags"])]
+        for product in products:
+            client.archive_product(product)
 
 
 if __name__ == "__main__":
