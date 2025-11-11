95cb86c7bc3c30bbaca220018c596b983e8cee3a
taro <taro.arumakan@gmail.com>
2025-11-11 13:38:33 +0900

    correct header length

--- brands/apricotstudios/update_size_table_html.py
+++ brands/apricotstudios/update_size_table_html.py
@@ -1,39 +1,19 @@
 import logging
 import utils
-from brands.apricotstudios.product_create import (
-    product_info_list_from_sheet_color_and_size,
-    text_to_html_tables_and_paragraphs,
-)
-from product_metafields_update_size_table_html import update_size_table_html_metafield
-from product_description_include_metafield_value import (
-    update_description_include_metafield_value,
-)
+from brands.apricotstudios.client import ApricotStudiosClient
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    client = utils.client("apricot-studios")
-    product_info_list = product_info_list_from_sheet_color_and_size(
-        client, client.sheet_id, "Products Master"
-    )
+    client = ApricotStudiosClient()
+    client.sheet_id = "1QOs5sQC7GP_-A4sljYekbI4UWT2ibxQ01HBxcs3BUPc"
+    product_info_list = client.product_info_list_from_sheet("9.25 25Autumn(2nd)")
     for product_info in product_info_list:
-        if product_info["title"] in [
-            "SUMMER MOTIVE SOCKS 3pcs",
-        ]:
-            size_text = product_info.get("size_text")
-            if size_text:
-                size_table_html = text_to_html_tables_and_paragraphs(size_text)
-                update_size_table_html_metafield(
-                    client, product_info["title"], size_table_html
-                )
-                update_description_include_metafield_value(
-                    client, product_info["title"]
-                )
-            else:
-                logging.warning(
-                    f"No size information found for {product_info['title']}"
-                )
+        title = product_info["title"]
+        size_table_html = client.get_size_field(product_info)
+        product_id = client.product_id_by_title(title)
+        client.update_size_table_html_metafield(product_id, size_table_html)
 
 
 if __name__ == "__main__":
