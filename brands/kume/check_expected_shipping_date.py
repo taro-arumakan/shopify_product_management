2c962c82f44296c7b79cef879ce37181ea748f8b
taro <taro.arumakan@gmail.com>
2025-11-11 16:53:25 +0900

    simple check script

---
+++ brands/kume/check_expected_shipping_date.py
@@ -0,0 +1,45 @@
+import datetime
+import utils
+
+
+def get_date_metafield_value(metafields, key):
+    if metafield := [m for m in metafields if m["key"] == key]:
+        return datetime.datetime.fromisoformat(metafield[0]["value"]).date()
+
+
+def main():
+    client = utils.client("kume")
+    products = client.products_by_query("status:'ACTIVE'")
+
+    products_outdated = []
+    variants_outdated = []
+
+    for product in products:
+        if expected_shipping_date := get_date_metafield_value(
+            product["metafields"]["nodes"], "expected_shipping_date"
+        ):
+            if expected_shipping_date <= datetime.date.today():
+                products_outdated.append((expected_shipping_date, product["title"]))
+        for variant in product["variants"]["nodes"]:
+            if variant_expected_shipping_date := get_date_metafield_value(
+                variant["metafields"]["nodes"], "variant_expected_shipping_date"
+            ):
+                if variant_expected_shipping_date < datetime.date.today():
+                    variants_outdated.append(
+                        (variant_expected_shipping_date, variant["displayName"])
+                    )
+
+    if products_outdated:
+        print("products with outdated expected shpping date")
+        for d, t in sorted(products_outdated, key=lambda x: x[0]):
+            print(f"{d}\t{t}")
+        print()
+
+    if variants_outdated:
+        print("variants with outdated expected shpping date")
+        for d, t in sorted(variants_outdated, key=lambda x: x[0]):
+            print(f"{d}\t{t}")
+
+
+if __name__ == "__main__":
+    main()
