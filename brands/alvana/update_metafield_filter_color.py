e33030a70c460d889b60d5255b457197293fc00c
taro <taro.arumakan@gmail.com>
2025-11-17 19:33:44 +0900

    one time update filter color metafield. check outliers

---
+++ brands/alvana/update_metafield_filter_color.py
@@ -0,0 +1,41 @@
+import logging
+import helpers.exceptions
+from brands.alvana.client import AlvanaClient
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    client = AlvanaClient()
+    product_info_list = client.product_info_list_from_sheet("Product Master")
+    for product_info in product_info_list:
+        for option in product_info["options"]:
+            filter_color = option["filter_color"]
+            for o2 in option["options"]:
+                sku = o2["sku"]
+                try:
+                    variant = client.variant_by_sku(sku)
+                    variant_id = variant["id"]
+                    product_id = variant["product"]["id"]
+                    client.update_variant_metafield(
+                        product_id, variant_id, "custom", "filter_color", filter_color
+                    )
+                except helpers.exceptions.NoVariantsFoundException as e:
+                    print(f"{product_info['title']}: {sku}")
+
+    products = client.products_by_query("status:'ACTIVE'")
+    for product in products:
+        for variant in product["variants"]["nodes"]:
+            filter_color_field = [
+                m for m in variant["metafields"]["nodes"] if m["key"] == "filter_color"
+            ]
+            if not filter_color_field:
+                print(f"missing filter color: {product['title']}")
+            else:
+                filter_color = filter_color_field[0]["value"]
+                if filter_color in ["GLAY", "INK BLACK", "SEAWEED"]:
+                    print(f"{product['title']}: {filter_color}")
+
+
+if __name__ == "__main__":
+    main()
