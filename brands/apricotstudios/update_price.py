ed2a45d755b06362b28a442a489d912bf32a10e8
taro-arumakan <taro.arumakan@gmail.com>
2025-11-26 15:56:02 +0900

    correct prices

---
+++ brands/apricotstudios/update_price.py
@@ -0,0 +1,24 @@
+import utils
+
+
+def main():
+    client = utils.client("apricot")
+
+    pil = client.product_info_list_from_sheet("11.20 25Winter_clone")
+
+    for pi in pil:
+        product = client.product_by_title(pi["title"])
+        compare_at_prices, prices, skus = [], [], []
+        for o1 in pi["options"]:
+            for o2 in o1["options"]:
+                compare_at_price = o2["price"]
+                compare_at_prices.append(compare_at_price)
+                prices.append(int(int(compare_at_price) * 0.9))
+                skus.append(o2["sku"])
+        client.update_variant_price_by_skus(
+            product["id"], skus=skus, prices=prices, compare_at_prices=compare_at_prices
+        )
+
+
+if __name__ == "__main__":
+    main()
