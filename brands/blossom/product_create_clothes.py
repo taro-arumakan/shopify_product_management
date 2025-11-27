7b5278939d9bc816b27d03540595820a308f2531
taro-arumakan <taro.arumakan@gmail.com>
2025-11-27 18:41:05 +0900

    process clothes drop 6

--- brands/blossom/product_create_clothes.py
+++ brands/blossom/product_create_clothes.py
@@ -12,17 +12,16 @@ def main():
 
     client.sanity_check_sheet(sheet_name)
 
-    # import zoneinfo
+    import zoneinfo
 
-    # scheduled_time = datetime.datetime(
-    #     2025, 10, 30, 12, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
-    # )
-    # scheduled_time = None
-    # client.process_product_info_list_to_products(
-    #     product_info_list,
-    #     additional_tags=["25_drop4", "New Arrival"],
-    #     scheduled_time=scheduled_time,
-    # )
+    scheduled_time = datetime.datetime(
+        2025, 12, 1, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+    )
+    client.process_sheet_to_products(
+        sheet_name=sheet_name,
+        additional_tags=["25_drop6", "New Arrival"],
+        scheduled_time=scheduled_time,
+    )
 
 
 if __name__ == "__main__":
