b381ff453dadfea83152d04676bde8aba1f18fbc
kentaro kimoto <exiler2411k@gmail.com>
2025-12-17 11:51:06 +0900

    origin/CEC-154-blossom-12-19-7th-drop

--- brands/blossom/product_create_shoes.py
+++ brands/blossom/product_create_shoes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import logging
 from brands.blossom.client import BlossomClientShoes
 
@@ -6,10 +7,18 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     client = BlossomClientShoes
-    sheet_name = "shoes"
+    sheet_name = "shoes(drop7)"
     client.sanity_check_sheet(sheet_name)
+
+    import zoneinfo
+
+    scheduled_time = datetime.datetime(
+        2025, 12, 19, 11, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+    )
     client.process_sheet_to_products(
-        sheet_name, additional_tags=["25_drop4", "New Arrival"]
+        sheet_name, 
+        additional_tags=["25_drop7", "New Arrival"],
+        scheduled_time=scheduled_time,
     )
 
 
