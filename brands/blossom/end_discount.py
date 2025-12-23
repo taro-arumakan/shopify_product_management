eb89c83deea51029de39b0e35eacf6380dc778c8
taro-arumakan <taro@sniarti.fi>
2025-12-23 16:22:22 +0900

    CEC-153: scheduled.

--- brands/blossom/end_discount.py
+++ brands/blossom/end_discount.py
@@ -9,7 +9,7 @@ def main():
     products = client.products_by_query(
         "tag_not:'25_drop7' AND tag_not:'25_drop8' AND status:'ACTIVE'"
     )
-    client.revert_variant_prices(products, testrun=True)
+    client.revert_variant_prices(products, testrun=False)
 
 
 if __name__ == "__main__":
