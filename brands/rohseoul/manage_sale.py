7e000daa48c7f58eba4e12909ad52b6fe5260a50
kentaro kimoto <exiler2411k@gmail.com>
2025-12-25 15:24:14 +0900

    add start_end_discounts_2025_new_year

--- brands/rohseoul/manage_sale.py
+++ brands/rohseoul/manage_sale.py
@@ -1,31 +1,20 @@
+from brands.rohseoul.client import RohseoulClient
 import utils
 import pandas as pd
 
 
-def start_2026_new_year_sale(testrun=True):
-    client = utils.client("ssil")
-    rows = client.worksheet_rows(
-        sheet_id="1uYE0j-LZxRzkXbe1Hlp8eB1J02mKFdFirtogytSmfcY", sheet_title="シート1"
-    )
-    df = pd.DataFrame(columns=["title", "sku"], data=rows)
-    skus = df["sku"].tolist()
-    variants = [client.variant_by_sku(sku) for sku in skus]
-    discounted_prices_by_variant_id = {
-        v["id"]: int(int(v["price"]) * 0.85) for v in variants
-    }
-    client.update_variant_prices_by_dict(
-        variants=variants,
-        new_prices_by_variant_id=discounted_prices_by_variant_id,
-        testrun=testrun,
-    )
+def start_end_discounts_2025_new_year(testrun=True, start_or_end="end"):
+    client = RohseoulClient()
+    products = client.products_by_query()
 
-
-def end_2026_new_year_sale(testrun=True):
-    client = utils.client("ssil")
-    rows = client.worksheet_rows(
-        sheet_id="1uYE0j-LZxRzkXbe1Hlp8eB1J02mKFdFirtogytSmfcY", sheet_title="シート1"
-    )
-    df = pd.DataFrame(columns=["title", "sku"], data=rows)
-    skus = df["sku"].tolist()
-    variants = [client.variant_by_sku(sku) for sku in skus]
-    client.revert_variant_prices(variants, testrun=testrun)
+    if start_or_end == "end":
+        client.revert_product_prices(products, testrun=testrun)
+    else:
+        new_prices_by_variant_id = {
+            v["id"]: int(int(v["compareAtPrice"] or v["price"]) * 0.9)
+            for p in products
+            for v in p["variants"]["nodes"]
+        }
+        client.update_product_prices_by_dict(
+            products, new_prices_by_variant_id=new_prices_by_variant_id, testrun=testrun
+        )
