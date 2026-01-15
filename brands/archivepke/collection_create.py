f37174d3ac3dfa905c42fc2d50eaa0e327a26b3b
kentaro kimoto <exiler2411k@gmail.com>
2026-01-15 11:15:24 +0900

    CEC-208-archivepke-1-23-1-31-new-year-appreciation-promoti

--- brands/archivepke/collection_create.py
+++ brands/archivepke/collection_create.py
@@ -4,24 +4,20 @@ import utils
 logging.basicConfig(level=logging.INFO)
 
 skus = [
-    "OVBAX26008PDB",
-    "OVBAX26007SDB",
-    "OVBAX26007SAL",
-    "OVBAX25210SCR",
     "OVBAX25004BLK",
-    "OVBAX25001BLK",
-    "OVBAX25002BLK",
-    "OVBAX25010BLK",
-    "OVBAX25102BLK",
     "OVBAX25005BLK",
-    "OVBAX25012SDB",
+    "OVBAX25229DKB",
+    "OVBAX26004SBK",
+    "OVBAX26004SCR",
+    "OVBAX25115BLK",
+    "OVBRX25102BLK",
 ]
 
 
 def main():
     client = utils.client("archive-epke")
     product_ids = {client.product_id_by_sku(sku) for sku in skus}
-    client.collection_create_by_product_ids("25 Christmas promotion", product_ids)
+    client.collection_create_by_product_ids("26 New Year Appreciation Promotion", product_ids)
 
 
 if __name__ == "__main__":
