e92c15fe37629ff545ace305ad6ea68dedaefc40
kentaro kimoto <exiler2411k@gmail.com>
2026-01-19 09:35:47 +0900

    CEC-180-blossom-1-23-2-1-outer-sale-30-off

--- brands/blossom/manage_sale.py
+++ brands/blossom/manage_sale.py
@@ -2,16 +2,16 @@ from brands.blossom.client import BlossomClient
 import utils
 import pandas as pd
 
-
-def start_end_discounts(testrun=True, start_or_end="end"):
+# Blossom 1/23 - 2/1 outer sale 30% off
+def start_end_discounts(testrun=True, start_or_end="start"):
     client = BlossomClient()
-    products = client.products_by_query()
+    products = client.products_by_query("tag:'OUTER' AND tag_not:'drop11' AND tag_not:'drop12'")
 
     if start_or_end == "end":
         client.revert_product_prices(products, testrun=testrun)
     else:
         new_prices_by_variant_id = {
-            v["id"]: int(int(v["compareAtPrice"] or v["price"]) * 0.8)
+            v["id"]: int(int(v["compareAtPrice"] or v["price"]) * 0.7)
             for p in products
             for v in p["variants"]["nodes"]
         }
@@ -21,7 +21,7 @@ def start_end_discounts(testrun=True, start_or_end="end"):
 
 
 def main():
-    start_end_discounts(testrun=False, start_or_end="start")
+    start_end_discounts()
     # start_end_discounts(testrun=False, start_or_end="end")
 
 
