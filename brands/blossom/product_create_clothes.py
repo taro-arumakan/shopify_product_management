56db1a2bfd335133e280797832772a262dcc59e9
taro-arumakan <taro@sniarti.fi>
2026-01-22 15:49:25 +0900

    CEC-215: - update blossom column mapping - control removal of new badges by vendor. remove irrelevant overrides.

--- brands/blossom/product_create_clothes.py
+++ brands/blossom/product_create_clothes.py
@@ -8,25 +8,25 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     client = BlossomClientClothes()
-    sheet_names = ["clothes(drop9)", "clothes(drop10)"]
-    drop_tags = ["drop9", "drop10"]
+    sheet_names = ["clothes(drop11)", "clothes(drop12)", "clothes(jp exclusive)"]
+    drop_tags = ["drop11", "drop12", "jp_exclusive"]
 
     import zoneinfo
 
     scheduled_time = datetime.datetime(
-        2026, 1, 14, 18, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 1, 26, 18, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
     for sheet_name, drop_tag in zip(sheet_names, drop_tags):
         client.sanity_check_sheet(sheet_name)
 
-        client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = sheet_name == "clothes(drop9)"
+        client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = sheet_name == "clothes(drop11)"
 
-        client.process_sheet_to_products(
-            sheet_name=sheet_name,
-            additional_tags=[drop_tag, "New Arrival"],
-            scheduled_time=scheduled_time,
-        )
+        # client.process_sheet_to_products(
+        #     sheet_name=sheet_name,
+        #     additional_tags=[drop_tag, "New Arrival"],
+        #     scheduled_time=scheduled_time,
+        # )
 
 
 if __name__ == "__main__":
