2a0434c9da1447834d66992955ea590e03c5a2fb
taro-arumakan <taro@sniarti.fi>
2026-01-22 13:15:32 +0900

    CEC-215: process a gift item

--- brands/ssil/product_create.py
+++ brands/ssil/product_create.py
@@ -7,13 +7,14 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    sheet_name = "HOLIDAY - correct URL"
+    sheet_name = "GIFT"
     client = SsilClientMaterialOptionOnly()
     client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
     client.sanity_check_sheet(sheet_name)
     scheduled_time = datetime.datetime(
         2025, 12, 4, 10, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
+    scgheduled_time = None
     client.process_sheet_to_products(sheet_name, scheduled_time=scheduled_time)
 
 
