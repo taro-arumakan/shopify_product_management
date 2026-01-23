29a9fa6467ef9f762e2cf18dfefd3f2bd30babd6
taro-arumakan <taro@sniarti.fi>
2026-01-23 15:26:33 +0900

    CEC-207: move sanity check methods to a mixin. move product_info_list method to brand cleint base. add check description. add check drive image. query skus in one go. clear drive_link_cache before populating product info list

--- brands/brandclientbase.py
+++ brands/brandclientbase.py
@@ -3,12 +3,13 @@ import json
 import logging
 import pathlib
 import pandas as pd
+from brands.sanity_checks import SanityChecks
 from helpers.client import Client
 
 logger = logging.getLogger(__name__)
 
 
-class BrandClientBase(Client):
+class BrandClientBase(Client, SanityChecks):
     SHOPNAME = ""
     VENDOR = ""
     LOCATIONS = []
@@ -40,6 +41,7 @@ class BrandClientBase(Client):
         raise NotImplementedError
 
     def product_info_list_from_sheet(self, sheet_name, handle_suffix=None):
+        self.drive_link_cache = {}  # repopulate drive link cache
         return self.to_products_list(
             self.sheet_id,
             sheet_name,
@@ -189,6 +191,19 @@ class BrandClientBase(Client):
         if self.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS:
             self.remove_existing_new_proudct_tags()
             self.remove_existing_new_badges()
+
+    def process_product_info_list_to_products(
+        self,
+        product_info_list,
+        additional_tags,
+        scheduled_time=None,
+        handle_suffix=None,
+    ):
+        for product_info in product_info_list:
+            self.create_product_from_product_info(product_info, additional_tags)
+            self.process_product_images(product_info, handle_suffix)
+        self.update_stocks(product_info_list)
+        self.publish_products(product_info_list, scheduled_time=scheduled_time)
 
     def colors_from_product_info_list(self, product_info_list):
         if "options" in product_info_list[0]:
---
+++ brands/sanity_checks.py
@@ -0,0 +1,123 @@
+import collections
+import logging
+from helpers.exceptions import (
+    NoVariantsFoundException,
+    NoProductsFoundException,
+    MultipleProductsFoundException,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class SanityChecks:
+
+    def check_size_field(self, product_info_list, raise_on_error=True):
+        res = []
+        for product_info in product_info_list:
+            try:
+                size_text = self.get_size_field(product_info)
+            except Exception as e:
+                m = f"Error formatting size text for {product_info['title']}: {e}"
+                if raise_on_error:
+                    logger.error(m)
+                    raise
+                else:
+                    res.append(m)
+            else:
+                if raise_on_error:
+                    print(size_text)
+        return res
+
+    def check_description(self, product_info_list, raise_on_error=True):
+        res = []
+        for product_info in product_info_list:
+            try:
+                description_html = self.get_description_html(product_info)
+            except Exception as e:
+                m = f"Error formatting description for {product_info['title']}: {e}"
+                if raise_on_error:
+                    logger.error(m)
+                    raise
+                else:
+                    res.append(m)
+            else:
+                if raise_on_error:
+                    print(description_html)
+        return res
+
+    def check_drive_image(self, product_info_list):
+        res = []
+        for product_info in product_info_list:
+            try:
+                drive_ids, _ = self.populate_drive_ids_and_skuss(product_info)
+                for drive_id in drive_ids:
+                    image_details = self.get_drive_image_details(drive_id)
+                    if not image_details:
+                        res.append(
+                            f"Missing or inaccessible drive image for {product_info['title']}: {drive_id}"
+                        )
+            except Exception as e:
+                res.append(
+                    f"Error checking drive images for {product_info['title']}: {e}"
+                )
+        return res
+
+    def product_info_to_skus(self, product_info):
+        options = self.populate_option_dicts(product_info)
+        return [o["sku"] for o in options]
+
+    def check_sku_duplicates(self, product_info_list):
+        skus = sum([self.product_info_to_skus(pi) for pi in product_info_list], [])
+        counts_by_sku = collections.Counter(skus)
+        counts_by_sku = {
+            sku: count for sku, count in counts_by_sku.items() if count > 1
+        }
+        if counts_by_sku:
+            m = "\n".join(
+                ": ".join(map(str, [sku, count]))
+                for sku, count in counts_by_sku.items()
+            )
+            raise RuntimeError(f"Duplicate SKUs found:\n{m}")
+
+    def check_existing_skus(self, product_info_list):
+        res = []
+        skus = sum((self.product_info_to_skus(pi) for pi in product_info_list), [])
+        query = query = " OR ".join(f"sku:'{sku}'" for sku in skus)
+        exists = self.product_variants_by_query(query)
+        for r in exists:
+            res.append(f"Existing SKU found: {r['product']['title']} - {r['sku']}")
+        return res
+
+    def check_existing_products(self, product_info_list):
+        res = []
+        for pi in product_info_list:
+            try:
+                checking = "handle" if "handle" in pi else "title"
+                func = getattr(self, f"product_by_{checking}")
+                param = pi[checking]
+                func(param)
+            except NoProductsFoundException:
+                pass
+            except MultipleProductsFoundException:
+                res.append(f"Existing product found by {checking}: {param}")
+            else:
+                res.append(f"Existing product found by {checking}: {param}")
+        return res
+
+    def sanity_check_product_info_list(self, product_info_list):
+        res = []
+        try:
+            self.check_sku_duplicates(product_info_list)
+        except RuntimeError as e1:
+            logger.error(e1)
+            res.append(e1)
+        res = self.check_existing_skus(product_info_list)
+        res += self.check_existing_products(product_info_list)
+        res += self.check_size_field(product_info_list, raise_on_error=False)
+        res += self.check_description(product_info_list, raise_on_error=False)
+        res += self.check_drive_image(product_info_list)
+        for r in res:
+            logger.error(r)
+
+        if res:
+            raise RuntimeError("Failed sanity check")
