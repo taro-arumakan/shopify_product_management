335f95b69f15bb01f91a30c0de54db8207c3d697
taro-arumakan <taro@sniarti.fi>
2026-01-23 15:55:56 +0900

    CEC-207: process drop 11, 12, jp exclusive

--- brands/blossom/product_create_clothes.py
+++ brands/blossom/product_create_clothes.py
@@ -1,13 +1,12 @@
-import datetime
 import logging
+
+logging.basicConfig(level=logging.INFO)
+
+import datetime
 from brands.blossom.client import BlossomClientClothes
 
 
-logging.basicConfig(level=logging.DEBUG)
-
-
 def main():
-    client = BlossomClientClothes()
     sheet_names = ["clothes(drop11)", "clothes(drop12)", "clothes(jp exclusive)"]
     drop_tags = ["drop11", "drop12", "jp_exclusive"]
 
@@ -17,16 +16,23 @@ def main():
         2026, 1, 26, 18, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
-    for sheet_name, drop_tag in zip(sheet_names, drop_tags):
-        client.sanity_check_sheet(sheet_name)
+    client = BlossomClientClothes()
 
+    for sheet_name in sheet_names:
+        try:
+            client.sanity_check_sheet(sheet_name)
+        except RuntimeError as e:
+            logging.error(f"Sanity check failed for {sheet_name}: {e}")
+
+    for sheet_name, drop_tag in zip(sheet_names, drop_tags):
         client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = sheet_name == "clothes(drop11)"
 
-        # client.process_sheet_to_products(
-        #     sheet_name=sheet_name,
-        #     additional_tags=[drop_tag, "New Arrival"],
-        #     scheduled_time=scheduled_time,
-        # )
+        client.process_sheet_to_products(
+            sheet_name=sheet_name,
+            additional_tags=[drop_tag, "New Arrival"],
+            scheduled_time=scheduled_time,
+            ignore_product_titles=["SOFFI CASHMERE V-NECK KNIT"],
+        )
 
 
 if __name__ == "__main__":
