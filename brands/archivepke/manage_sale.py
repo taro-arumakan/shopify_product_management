83d2ecd79b5c36625ff769408fd93e1eb59b5952
kentaro kimoto <exiler2411k@gmail.com>
2026-02-10 14:39:34 +0900

    CEC-240-archivepke-2-21-2-28-new-color-15-off

--- brands/archivepke/manage_sale.py
+++ brands/archivepke/manage_sale.py
@@ -4,19 +4,31 @@ from brands.archivepke.client import ArchivepkeClient
 def start_end_discounts(testrun=True, start_or_end="end"):
     client = ArchivepkeClient()
 
-    # New Year Appreciation Promotion
+    # 26SS NEW COLOR PROMOTION 2.21~2.28 ALL 15% OFF
     skus = [
-        "OVBAX25004BLK",
-        "OVBAX25005BLK",
-        "OVBAX25229DKB",
-        "OVBAX26004SBK",
-        "OVBAX26004SCR",
-        "OVBAX25115BLK",
-        "OVBRX25102BLK",
+        "OVBAX26001CMI",
+        "OVBAX26001GGR",
+        "OVBAX26001GMB",
+        "OVBAX26001GOB",
+        "OVBAX26001HRD",
+        "OVBAX26001OBG",
+        "OVBAX26002IGR",
+        "OVBAX26002MBR",
+        "OVBAX26002OBG",
+        "OVBAX26004GGR",
+        "OVBAX26004MBR",
+        "OVBAX26004OBG",
+        "OVBAX26105IVO",
+        "OVBAX26107BRW",
+        "OVBAX26107WBL",
+        "OVBAX26108DKB",
+        "OVBAX26108DPK",
+        "OVBAX26108WHT",
+        "OVBAX26104BRW",
     ]
 
     variants = [client.variant_by_sku(sku) for sku in skus]
-
+ 
     if start_or_end == "end":
         client.revert_variant_prices(variants, testrun=testrun)
     else:
