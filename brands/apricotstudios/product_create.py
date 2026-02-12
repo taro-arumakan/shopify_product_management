4e1b2959317436f07fcf9d13558425d750446986
taro-arumakan <taro@sniarti.fi>
2026-02-12 17:10:32 +0900

    CEC-232: process/reprocess 26 spring 1st

--- brands/apricotstudios/product_create.py
+++ brands/apricotstudios/product_create.py
@@ -1,35 +1,55 @@
+import datetime
 import logging
 from brands.apricotstudios.client import ApricotStudiosClient
 
 logging.basicConfig(level=logging.INFO)
 
-# def parse_a_size_line(size_line):
-#     pattern = re.compile(r"(\S+?)：([\d.]+(?: cm)?)")
-#     if not (size := pattern.findall(size_line)):
-#         return ("サイズ", size_line.replace("サイズ ", "").strip())
-#     return size[0]
-
-
-# def parse_table_text_to_html(table_text):
-#     table_text_all_sizes = filter(None, table_text.split("\n\n"))
-#     header_value_pairs = []
-#     for tt in table_text_all_sizes:
-#         header_value_pairs.append([parse_a_size_line(line) for line in tt.split("\n")])
-#     headers = [""] + [p[0] for p in header_value_pairs[0][1:]]
-#     values = [[p[1] for p in pairs] for pairs in header_value_pairs]
-#     return utils.Client.generate_table_html(headers, values)
-
 
 def main():
     client = ApricotStudiosClient(
-        "gid://shopify/Product/9181957095680", "1jOg_no7MS8tGwMLKvOpodPg58nWKXSgX"
+        "gid://shopify/Product/9344675578112",
+        product_detail_images_folder_id="1fCXufym34XgTt80wq0ADVrQ-3SWXZQIZ",
     )
-    sheet_name = "[Spring] 2/25"
-    client.sanity_check_sheet(sheet_name)
-    # client.process_sheet_to_products(
-    #     sheet_name, additional_tags=["25_autumn_2nd", "New Arrival"]
-    # )
+    sheet_name = "[Spring_1st] 2/25"
+
+    import zoneinfo
+
+    scheduled_time = datetime.datetime(
+        2026, 2, 25, 10, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+    )
+
+    # client.sanity_check_sheet(sheet_name)
+    client.process_sheet_to_products(
+        sheet_name,
+        restart_at_product_title="Bliss Collar Work Jacket",
+        additional_tags=["26_spring_1st", "New Arrival"],
+        scheduled_time=scheduled_time,
+    )
+
+
+def reprocess():
+    """after creating failed products, reprocess required fields"""
+    client = ApricotStudiosClient(
+        "gid://shopify/Product/9344675578112",
+        product_detail_images_folder_id="1fCXufym34XgTt80wq0ADVrQ-3SWXZQIZ",
+    )
+    sheet_name = "[Spring_1st] 2/25"
+    product_inputs = client.product_inputs_by_sheet_name(sheet_name)
+
+    import zoneinfo
+
+    scheduled_time = datetime.datetime(
+        2026, 2, 25, 10, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+    )
+
+    for product_input in product_inputs:
+        product_id = client.product_id_by_title(product_input["title"])
+        client.update_description_include_metafield_values(product_id)
+    client.update_stocks(product_inputs)
+    client.publish_products(product_inputs, scheduled_time=scheduled_time)
+    client.post_process_product_inputs(product_inputs)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    reprocess()
