dfba8dd432a1f38c984be9a61fd934c698adb0c4
taro-arumakan <taro@sniarti.fi>
2026-02-17 14:30:14 +0900

    CEC-241: process Gold Line

--- brands/ssil/product_create.py
+++ brands/ssil/product_create.py
@@ -1,21 +1,24 @@
 import datetime
 import logging
 import zoneinfo
-from brands.ssil.client import SsilClient, SsilClientMaterialOptionOnly
+from brands.ssil.client import SsilClientGoldLine
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    sheet_name = "GIFT"
-    client = SsilClientMaterialOptionOnly()
-    client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
-    client.sanity_check_sheet(sheet_name)
+    sheet_name = "GOLD PRODUCT"
+    client = SsilClientGoldLine()
+    # client.sanity_check_sheet(sheet_name)
     scheduled_time = datetime.datetime(
-        2025, 12, 4, 10, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 2, 20, 12, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
-    scgheduled_time = None
-    client.process_sheet_to_products(sheet_name, scheduled_time=scheduled_time)
+    client.process_sheet_to_products(
+        sheet_name,
+        scheduled_time=scheduled_time,
+        additional_tags=["26_gold_line"],
+        restart_at_product_title="[G]CIRCLE CHAIN R",
+    )
 
 
 if __name__ == "__main__":
