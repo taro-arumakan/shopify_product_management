25d9e1ddfd573b0566e045045eb53bff3bd5cda9
taro-arumakan <taro@sniarti.fi>
2026-02-20 20:29:09 +0900

    CEC-249: process Home Square Room Shoes / Towel

--- brands/gbh/product_create_home.py
+++ brands/gbh/product_create_home.py
@@ -6,40 +6,16 @@ from brands.gbh.client import GbhHomeClient
 logging.basicConfig(level=logging.INFO)
 
 
-def fix_price():
-    client = GbhHomeClient(use_simple_size_format=True)
-    product_inputs = client.product_inputs_by_sheet_name("bedding / ROBE (SIZE+COLOR)")
-    price_by_sku = {
-        v2["sku"]: v2["price"]
-        for p in product_inputs
-        for v1 in p["options"]
-        for v2 in v1["options"]
-    }
-    variants = [client.variant_by_sku(sku) for sku in price_by_sku.keys()]
-    new_prices_by_variant_id = {
-        v["id"]: p for v, p in zip(variants, price_by_sku.values())
-    }
-    client.update_variant_prices_by_dict(
-        variants=variants,
-        new_prices_by_variant_id=new_prices_by_variant_id,
-        testrun=False,
-    )
-
-
 def main():
 
-    client = GbhHomeClient(use_simple_size_format=True)
+    client = GbhHomeClient(use_simple_size_format=False)
     client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
-    sheet_name = "bedding / ROBE (SIZE+COLOR)"
-
-    # client = GbhClientColorOptionOnly()
-    # client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
-    # sheet_name = "bedding / ROBE (COLOR ONLY)"
+    sheet_name = "HOME_SQUARE ROOM SHOES / TOWEL"
 
     client.sanity_check_sheet(sheet_name)
 
     scheduled_time = datetime.datetime(
-        2026, 1, 16, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 2, 27, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
     client.process_sheet_to_products(
@@ -48,4 +24,4 @@ def main():
 
 
 if __name__ == "__main__":
-    fix_price()
+    main()
