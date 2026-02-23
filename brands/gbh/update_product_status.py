07e25ba65ec583e10463c4f737a4cecefa3bc1d9
taro <taro.arumakan@gmail.com>
2026-02-23 14:05:03 +0900

    products to draft status

--- brands/gbh/update_product_status.py
+++ brands/gbh/update_product_status.py
@@ -13,3 +13,17 @@ def archive_products(testrun=True):
         logging.info(f"Deactivating product: {product['title']} (id: {product_id})")
         if not testrun:
             client.update_product_status(product_id, "ARCHIVED")
+
+
+def products_to_draft(testrun=True):
+    client = utils.client("gbh")
+    products = client.products_by_collection_id("324132896967")
+    for product in products:
+        product_id = product["id"]
+        logging.info(f"Product to draft status: {product['title']} (id: {product_id})")
+        if not testrun:
+            client.update_product_status(product_id, "DRAFT")
+
+
+if __name__ == "__main__":
+    products_to_draft(testrun=False)
