b639a55f0b8ffb1bad04a32a43c539c0e5bae146
taro-arumakan <taro@sniarti.fi>
2026-02-26 15:16:33 +0900

    CEC-266: publish additional gold line products

--- brands/ssil/product_create.py
+++ brands/ssil/product_create.py
@@ -7,17 +7,18 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    sheet_name = "GOLD PRODUCT"
-    client = SsilClientGoldLine()
-    # client.sanity_check_sheet(sheet_name)
+    sheet_name = "gold確認必要"
+    client = SsilClientGoldLine(product_sheet_start_row=1)
+    client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
+    client.sanity_check_sheet(sheet_name)
     scheduled_time = datetime.datetime(
         2026, 2, 20, 12, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
+    scheduled_time = None
     client.process_sheet_to_products(
         sheet_name,
         scheduled_time=scheduled_time,
         additional_tags=["26_gold_line"],
-        restart_at_product_title="[G]CIRCLE CHAIN R",
     )
 
 
