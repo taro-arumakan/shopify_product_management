83ec695f01ce4a8a399006f2e6662246bea14932
taro <taro.arumakan@gmail.com>
2026-02-26 17:03:26 +0900

    update sale price as well

--- brands/blossom/add_variants.py
+++ brands/blossom/add_variants.py
@@ -12,6 +12,18 @@ def main():
     for product_input in product_inputs:
         client.add_variants_from_product_input(product_input)
 
+    for product_input in product_inputs:
+        product = client.product_by_title(product_input["title"])
+        new_variant_prices_by_variant_id = {
+            v["id"]: int(int(v["compareAtPrice"]) * 0.8)
+            for v in product["variants"]["nodes"]
+        }
+        client.update_product_prices_by_dict(
+            [product],
+            new_prices_by_variant_id=new_variant_prices_by_variant_id,
+            testrun=False,
+        )
+
 
 if __name__ == "__main__":
     main()
