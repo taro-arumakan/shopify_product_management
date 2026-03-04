44b9d6e864c02c485fbb97a311b238124af1c628
kentaro kimoto <exiler2411k@gmail.com>
2026-03-04 10:22:46 +0900

    add collection,sale create

--- brands/gbh/product_create_schedule.py
+++ brands/gbh/product_create_schedule.py
@@ -56,13 +56,140 @@ def create_cosmetic():
     )
 
 
+skus = [
+    "APC1PT050MBS",
+    "APC1PT050MBM",
+    "APC1PT040IDS",
+    "APC1PT040IDM",
+    "APB3PT040IDS",
+    "APB3PT040IDM",
+    "APB3PT040BKS",
+    "APB3PT040BKM",
+    "APC1PT130WHS",
+    "APC1PT130WHM",
+    "APB3PT040KHS",
+    "APB3PT040KHM",
+    "APC1PT080BKS",
+    "APC1PT080BKM",
+    "APC1PT080BES",
+    "APC1PT080BEM",
+    "APA3JK040BKFF",
+    "APB3JK010BRFF",
+    "APC1CT020BEFF",
+    "APC1TP110RDFF",
+    "APC1TP110WHFF",
+    "APC1TP110BKFF",
+    "APC1TP060LBFF",
+    "APC1TP060BLFF",
+    "APC1TP060BKFF",
+    "APC1TP150RDFF",
+    "APC1TP150CRFF",
+    "APC1TP150LBFF",
+    "APC1TP150NYFF",
+    "APC1SH020WHFF",
+    "APB1KN030GYFF",
+    "APB1KN030RDFF",
+    "APB1KN030BKFF",
+    "APB1KN030NYFF",
+    "APC1KN030BUFF",
+    "APB3KN070BKFF",
+    "APB3KN070NYFF",
+    "APB3KN070RDFF",
+    "APB3KN070IVFF",
+    "APC1KN040SBFF",
+    "APC1CD010BPFF",
+    "APC1CD010SBFF",
+    "APC1CD010MGFF",
+    "APC1CD010NYFF",
+    "APC1CD010BKFF",
+    "APC1CD010BSFF",
+    "APC1CD010GSFF",
+    "APB3BT020DWFF",
+    "APB3AC070YEFF",
+    "APB3AC070GRFF",
+    "APB3AC070OLFF",
+    "APB3AC070BLFF",
+    "APB3AC070IVFF",
+    "APB3AC070RDFF",
+    "APB3AC070CHFF",
+    "APB3AC070BKFF",
+    "APC1AC020CHFF",
+    "APC1AC020PUFF",
+    "APC1AC020RDFF",
+    "APC1AC030RDFF",
+    "APC1AC030BKFF",
+    "APC1AC030CHFF",
+    "APC1AC030YEFF",
+    "APC1AC030BRFF",
+    "8800256755561",
+    "8800256755936",
+    "8809855950155",
+    "APB1PT140BKS",
+    "APB1PT140BKM",
+    "APC1HZ020CHFF",
+    "APC1HZ020BKFF",
+    "APC1JP010WHFF",
+    "APC1JP010KHFF",
+    "APC1TP090BRFF",
+    "APC1TP090NYFF",
+    "APC1TP090MGFF",
+    "APC1TP100GYFF",
+    "APC1TP100BLFF",
+    "APC1TP140GYFF",
+    "APC1TP140BKFF",
+    "APC1TP140WHFF",
+    "APC1SK050WHFF",
+    "APC1SK050CKFF",
+    "APB3CD090IVFF",
+    "APB3CD090MGFF",
+    "APB3CD090RDFF",
+    "APB3CD090OLFF",
+    "APB3CD090BKFF",
+    "APC1PT110BKFF",
+    "APC1PT110RDFF",
+    "APC1PT110BPFF",
+    "APB3BG020BKFF",
+    "APB3BG010CCFF",
+    "APB3BG010BKFF",
+    "APC1AC040IDFF",
+    "APC1AC040BKFF",
+    "APC1AC040CHFF",
+    "APC1AC040RDFF",
+    "APC1AC040BEFF",
+    "APC1AC040WHFF",
+]
+
+
+def create_collection():
+    client = GbhClient()
+    product_ids = {client.product_id_by_sku(sku) for sku in skus}
+    client.collection_create_by_product_ids("GBH 3/10-3/15 sale", product_ids)
+
+
+def start_end_discounts(testrun=True, start_or_end="end"):
+    client = GbhClient()
+    variants = [client.variant_by_sku(sku) for sku in skus]
+ 
+    if start_or_end == "end":
+        client.revert_variant_prices(variants, testrun=testrun)
+    else:
+        new_prices_by_variant_id = {
+            v["id"]: int(int(v["compareAtPrice"] or v["price"]) * 0.9)
+            for v in variants
+        }
+        client.update_variant_prices_by_dict(
+            variants, new_prices_by_variant_id=new_prices_by_variant_id, testrun=testrun
+        )
+
+
 def main():
     archive_apparel_products()
     archive_cosmetic_products()
     create_26ss_color_only()
     create_26ss_color_size()
     create_cosmetic()
-
+    create_collection()
+    start_end_discounts(testrun=False, start_or_end="start")
 
 if __name__ == "__main__":
     main()
