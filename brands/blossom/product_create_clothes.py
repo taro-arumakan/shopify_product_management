86e5be1eeacb763110e81b1aaca76d1f22ee58a5
taro-arumakan <taro@sniarti.fi>
2026-03-06 20:25:08 +0900

    CEC-274: pre-rewrite check

--- brands/blossom/product_create_clothes.py
+++ brands/blossom/product_create_clothes.py
@@ -7,26 +7,26 @@ from brands.blossom.client import BlossomClientClothes
 
 
 def main():
-    sheet_name = "clothes(drop11)_SOFFI CASHMERE V-NECK KNIT"
-    drop_tag = "drop11"
+    sheet_name = "clothes(drop1) PS"
+    drop_tag = "2026_drop1"
 
-    import zoneinfo
+    client = BlossomClientClothes(product_sheet_start_row=1)
 
-    scheduled_time = datetime.datetime(
-        2026, 1, 26, 18, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
-    )
+    client.sanity_check_sheet(sheet_name, pre_rewrite=False)
 
-    client = BlossomClientClothes()
+    # # client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
 
-    client.sanity_check_sheet(sheet_name)
+    # import zoneinfo
 
-    client.REMOVE_EXISTING_NEW_PRODUCT_INDICATORS = False
+    # scheduled_time = datetime.datetime(
+    #     2026, 3, 10, 18, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+    # )
 
-    client.process_sheet_to_products(
-        sheet_name=sheet_name,
-        additional_tags=[drop_tag, "New Arrival"],
-        scheduled_time=scheduled_time,
-    )
+    # client.process_sheet_to_products(
+    #     sheet_name=sheet_name,
+    #     additional_tags=[drop_tag, "New Arrival"],
+    #     scheduled_time=scheduled_time,
+    # )
 
 
 if __name__ == "__main__":
