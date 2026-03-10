f6a5e1ee6caee26930d1e98bc4eff5ebd0a359c7
kentaro kimoto <exiler2411k@gmail.com>
2026-03-10 09:53:15 +0900

    CEC-271-kume-3-16-26ss-2nd-drop-10-off

--- brands/kume/product_create.py
+++ brands/kume/product_create.py
@@ -7,19 +7,20 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    sheet_name = "26SS_1次_2月23日"
-    client = KumeClient()
+    sheet_name = "26SS_2次_3月16日"
+    client = KumeClient(product_sheet_start_row=2)
 
-    filter_func = lambda product_input: product_input["release_date"] == "2026-02-23"
+    filter_func = lambda product_input: product_input["release_date"] == "2026-03-16"
 
     scheduled_time = datetime.datetime(
-        2026, 2, 23, 12, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 3, 16, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
     client.sanity_check_sheet(sheet_name, product_inputs_filter_func=filter_func)
 
     client.process_sheet_to_products(
         sheet_name,
+        additional_tags=["26SS_2nd_DROP", "New Arrival"],
         scheduled_time=scheduled_time,
         product_inputs_filter_func=filter_func,
     )
