bdb84e56f0835a16b6b0d1c0988fbd501419905a
taro <taro.arumakan@gmail.com>
2026-03-20 15:09:15 +0900

    upload to resources folder, with brand naming convention

--- brands/scripts/export_monthly_graphs.py
+++ brands/scripts/export_monthly_graphs.py
@@ -5,7 +5,7 @@ logging.basicConfig(level=logging.INFO)
 
 import utils
 
-parent_folder_id = "1qIl57QGB0TCgD_IsZ4vcFjxBbjZQVomN"
+parent_folder_id = "13e7ejhsYGaelUwwteM3SC6_aMrhEsZW4"
 
 
 def run(brand, report_year, report_month):
@@ -20,8 +20,8 @@ def run(brand, report_year, report_month):
         parent_folder_id=parent_folder_id,
         folder_name=f"{datetime.date(report_year, report_month, 1):%Y%m}",
     )
-    for i, graph in enumerate(graphs):
-        output_path = f"/tmp/{client.VENDOR}_{datetime.date(report_year, report_month, 1):%Y%m}_{str(i).zfill(2)}_{graph}_.png"
+    for graph in graphs:
+        output_path = f"/tmp/{brand}_{graph}.png"
         client.generate_monthly(
             getattr(client, f"generate_{graph}"),
             output_path=output_path,
@@ -38,19 +38,36 @@ def run(brand, report_year, report_month):
 def main():
     rundate = datetime.date.today()
     report_date = rundate.replace(day=1) - datetime.timedelta(days=1)
-    brands = ["apricot", "blossom", "archive", "gbh", "kume", "lememe", "roh", "ssil"]
+    brands = [
+        "Apricot Studios",
+        "BLOSSOM",
+        "Archivépke",
+        "GBH",
+        "KUMÉ",
+        "LEMEME",
+        "ROH SEOUL",
+        "SSIL",
+    ]
     for brand in brands:
         run(brand, report_date.year, report_date.month)
 
 
 def adhoc():
     year_months = [(2025, 11), (2025, 12), (2026, 1)]
-    brands = ["apricot", "blossom", "archive", "gbh", "kume", "lememe", "roh", "ssil"]
+    brands = [
+        "Apricot Studios",
+        "BLOSSOM",
+        "Archivépke",
+        "GBH",
+        "KUMÉ",
+        "LEMEME",
+        "ROH SEOUL",
+        "SSIL",
+    ]
     for year, month in year_months:
         for brand in brands:
             run(brand, year, month)
 
 
 if __name__ == "__main__":
-    adhoc()
     main()
