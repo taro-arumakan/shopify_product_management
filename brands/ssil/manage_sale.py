15c647edcb719b724a71b47ab34ccecb3dc9b3bd
kentaro kimoto <exiler2411k@gmail.com>
2026-04-13 09:26:24 +0900

    CEC-322-ssil-4-15-best-products-sale

--- brands/ssil/manage_sale.py
+++ brands/ssil/manage_sale.py
@@ -59,8 +59,89 @@ def start_end_new_clover_sale(testrun=True, start_or_end="end"):
         )
 
 
+def start_end_best_products_sale(testrun=True, start_or_end="end"):
+    skus = [
+        "SS24SRC200CG009ZZ",
+        "SS23FNC006CS011ZZ",
+        "SS23FNC006CS013ZZ",
+        "SS23FNC006CS015ZZ",
+        "SS23FNC006CS017ZZ",
+        "SS24SRC207CG019ZZ",
+        "24FEES09CBLL",
+        "24FEES09WBLL",
+        "24FEES09YBLL",
+        "24FEES10CBSS",
+        "24FEES10WBSS",
+        "24FEES10YBSS",
+        "SS20FEX005WBXXXMT",
+        "SS20FEX005YBXXXMT",
+        "24FRES03WS11",
+        "24FRES03WS13",
+        "24FRES03WS15",
+        "24FRES03WS17",
+        "24FRES03WS19",
+        "24FRES02WS09",
+        "24FRES02WS11",
+        "24FRES02WS13",
+        "24FRES02WS15",
+        "24FRES02WS17",
+        "24FRES02WS19",
+        "24FRES02YS09",
+        "24FRES02YS11",
+        "24FRES02YS13",
+        "24FRES02YS15",
+        "24FRES02YS17",
+        "24FRES02YS19",
+        "24FRES01WS09",
+        "24FRES01WS11",
+        "24FRES01WS13",
+        "24FRES01WS15",
+        "24FRES01WS17",
+        "24FRES01WS19",
+        "24FRES01YS09",
+        "24FRES01YS11",
+        "24FRES01YS13",
+        "24FRES01YS15",
+        "24FRES01YS17",
+        "24FRES01YS19",
+        "SS24SNC039WGLLLMT",
+        "25FNHR01WBFF",
+        "SS24SNC027WGFFFMT",
+        "SS24SNC027YGFFFMT",
+        "SS24SNC041WGFFFMT",
+        "SS24SNC041YGFFFMT",
+        "24FECV02WBFF",
+        "25SNCV03WBFF",
+        "25SNCV03YBFF",
+        "25SNES01NNSS",
+        "25SNES01CSFF",
+        "SS24SNC041WGFFFMT",
+        "SS24SNC041YGFFFMT",
+        "25FNBA09WSFF",
+        "25FNBA09YSFF",
+        "25SRCV02WS11",
+        "25SRCV02WS13",
+        "25SRCV02WS15",
+        "25SRCV02YS11",
+        "25SRCV02YS13",
+        "25SRCV02YS15",
+    ]
+
+    client = utils.client("ssil")
+    variants = [client.variant_by_sku(sku) for sku in skus]
+
+    if start_or_end == "end":
+        client.revert_variant_prices(variants=variants, testrun=testrun)
+    else:
+        new_prices_by_variant_id = {
+            v["id"]: int(int(v["compareAtPrice"] or v["price"]) * 0.8) for v in variants
+        }
+        client.update_variant_prices_by_dict(
+            variants=variants,
+            new_prices_by_variant_id=new_prices_by_variant_id,
+            testrun=testrun,
+        )
+
+
 if __name__ == "__main__":
-    start_2026_new_year_sale(testrun=True)
-    end_2026_new_year_sale(testrun=True)
-    start_end_new_clover_sale(testrun=True, start_or_end="start")
-    start_end_new_clover_sale(testrun=True, start_or_end="end")
+    start_end_best_products_sale(testrun=True, start_or_end="start")
