4f48756b656d982ac5c073041dbfba515cc157ad
kentaro kimoto <exiler2411k@gmail.com>
2026-04-20 13:04:15 +0900

    CEC-329-archivepke-4-22-26ss

--- brands/archivepke/product_create.py
+++ brands/archivepke/product_create.py
@@ -9,20 +9,24 @@ logging.basicConfig(level=logging.INFO)
 def main():
     handle_suffix = "26SS"
 
-    sheet_name = "2026.03.05 (26SS COLLECTION 1ST)"
-    client = ArchivepkeClient(product_sheet_start_row=3)
+    sheet_name = "2026.04.22(26SS Collection)"
+    client = ArchivepkeClient(
+        product_sheet_start_row=3,
+        remove_existing_new_product_indicators=True,
+        products_season_tag=handle_suffix,
+    )
 
     import zoneinfo
 
     scheduled_time = datetime.datetime(
-        2026, 3, 5, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 4, 22, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
     client.sanity_check_sheet(sheet_name, handle_suffix=handle_suffix)
     client.process_sheet_to_products(
         sheet_name=sheet_name,
         handle_suffix=handle_suffix,
-        additional_tags=["26SS", "New Arrival"],
+        additional_tags=["New Arrival"],
         scheduled_time=scheduled_time,
     )
 
