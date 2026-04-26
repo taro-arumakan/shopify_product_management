d706327833f8ffba5f6989f6fb282400a2e42a2d
kentaro kimoto <exiler2411k@gmail.com>
2026-04-26 15:00:33 +0900

    CEC-327-ssil-4-27-pendant

--- brands/ssil/product_create.py
+++ brands/ssil/product_create.py
@@ -7,19 +7,21 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    sheet_name = "NEW X ROW"
+    sheet_name = "Pendant Collection"
     client = SsilClient(
-        product_sheet_start_row=1, remove_existing_new_product_indicators=True
+        product_sheet_start_row=1,
+        remove_existing_new_product_indicators=True,
+        products_season_tag="26_0427_pendant_collection",
     )
     # filter_func = lambda pi: pi["title"] not in ["CLOVER BAND R"]
     client.sanity_check_sheet(sheet_name)
     scheduled_time = datetime.datetime(
-        2026, 3, 27, 12, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 4, 28, 10, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
     client.process_sheet_to_products(
         sheet_name,
         scheduled_time=scheduled_time,
-        additional_tags=["X", "New Arrival", "26_0327X-ROW"],
+        additional_tags=["New Arrival"],
     )
 
 
