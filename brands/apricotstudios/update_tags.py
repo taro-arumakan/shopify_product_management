5e6fc1349c8b2276102a08265576c1876454043d
kentaro kimoto <exiler2411k@gmail.com>
2026-04-28 17:54:25 +0900

    CEC-341-apricot-studios-streamline-category-filtering

--- brands/apricotstudios/update_tags.py
+++ brands/apricotstudios/update_tags.py
@@ -1,17 +1,18 @@
 import logging
-import pprint
-import utils
+from brands.apricotstudios.client import ApricotStudiosClient
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    client = utils.client("apricot-studios")
-    product_inputs = client.product_inputs_by_sheet_name("11.20 25Winter_clone")
-    for pi in product_inputs:
-        product = client.product_by_title(pi["title"])
-        tags = product["tags"] + ["25 Winter"]
-        pprint.pprint(client.update_product_tags(product["id"], tags))
+    client = ApricotStudiosClient()
+    products = client.products_by_query()
+    for product in products:
+        current_tags = product.get("tags", [])
+        normalized_tags = [client.normalize_tag(tag) for tag in current_tags if tag]
+        logging.info(f"normalizing tags: {product['title']}")
+        client.update_product_tags(product["id"], ",".join(normalized_tags))
+    logging.info(f"done. normalized {len(products)} products")
 
 
 if __name__ == "__main__":
