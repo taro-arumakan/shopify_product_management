d50d372321e0cecdcc15b56e08b652e1f1471193
kentaro kimoto <exiler2411k@gmail.com>
2026-05-08 08:34:40 +0900

    CEC-337-lememe-5-15-26-hot-summer

--- brands/lememe/product_create.py
+++ brands/lememe/product_create.py
@@ -6,22 +6,32 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    client = LememeClientApparel(
+    client = LememeClient(
         product_sheet_start_row=1,
         remove_existing_new_product_indicators=True,
-        products_season_tag="2026_0506_RTW_summer",
+        products_season_tag="2026_0515_bags",
     )
-    sheet_name = "0506_RTW_summer"
+    sheet_name = "0515_bags"
+    target_titles = {
+        "Sac Lume",
+        "Sac Clea",
+        "Sac Romain Nylon",
+        "Sac Benoit Black",
+    }
     import datetime
     import zoneinfo
 
     scheduled_time = datetime.datetime(
-        2026, 5, 6, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 5, 15, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
-    client.sanity_check_sheet(sheet_name)
+    client.sanity_check_sheet(
+        sheet_name,
+        product_inputs_filter_func=lambda pi: pi.get("title") in target_titles,
+    )
     client.process_sheet_to_products(
         sheet_name,
+        product_inputs_filter_func=lambda pi: pi.get("title") in target_titles,
         scheduled_time=scheduled_time,
     )
 
