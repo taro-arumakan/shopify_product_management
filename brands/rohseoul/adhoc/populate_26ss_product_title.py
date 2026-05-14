424783efa5d56e66ff328827af562df065db9074
taro-arumakan <taro@sniarti.fi>
2026-05-14 15:23:56 +0900

    CEC-359 guess product url

--- brands/rohseoul/adhoc/populate_26ss_product_title.py
+++ brands/rohseoul/adhoc/populate_26ss_product_title.py
@@ -1,6 +1,8 @@
+import functools
 import json
-import re
 import pathlib
+import re
+
 import utils
 from brands.rohseoul.publish_lookbook_article import populate_lookbook_products_dicts
 
@@ -13,14 +15,46 @@ path = (
 )
 
 
-def populate_product_content(title):
-    products = lookbook_products_dict[title]
+def variant_color(variant):
+    for so in variant["selectedOptions"]:
+        if so["name"] == "カラー":
+            return so["value"]
+
+
+@functools.lru_cache(maxsize=128)
+def products_by_title_cached(product_title):
+    return client.products_by_query(f"""title:"{product_title}" AND status:'ACTIVE'""")
+
+
+@functools.lru_cache(maxsize=128)
+def lookup_product_variant_url(product_and_color):
+    # Pattern explanation:
+    # ^(.*)         -> Group 1: Matches everything from the start (greedy)
+    # \s+           -> Matches the whitespace separator
+    # ([A-Z].*)     -> Group 2: Matches a capital letter followed by anything to the end
+    pattern = r"^(.*)\s+([A-Z].*)$"
+
+    match = re.search(pattern, product_and_color)
+    if match:
+        title = match.group(1)
+        color = match.group(2)
+        print(f"Title: {title} | Color: {color}")
+        products = products_by_title_cached(title)
+        for p in products:
+            for v in p["variants"]["nodes"]:
+                if variant_color(v).lower() == color.lower():
+                    return f"https://rohseoul.jp/products/{p['handle']}?variant={v['id'].rsplit("/", 1)[-1]}"
+
+
+def populate_product_content(section_title):
+    products = lookbook_products_dict[section_title]
     ress = []
-    for product, link in products:
+    for product_title, link in products:
         res = "<p>"
+        link = link or lookup_product_variant_url(product_title)
         if link:
             res += f'<a href="{link}">'
-        res += f"{product}"
+        res += f"{product_title}"
         if link:
             res += "</a>"
         res += "</p>"
@@ -47,6 +81,13 @@ def main():
 
     with open(path, "w") as of:
         of.write(json.dumps(d, indent=2, ensure_ascii=False))
+
+
+def check():
+    for k, v in lookbook_products_dict.items():
+        for product_title, _ in v:
+            print(product_title)
+            print(lookup_product_variant_url(product_title))
 
 
 if __name__ == "__main__":
