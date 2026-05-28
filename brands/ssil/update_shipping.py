170691de0d8796973d05b8798f861cd1bfbb1bda
taro-arumakan <taro@sniarti.fi>
2026-05-28 16:23:50 +0900

    CEC-362: testrun

---
+++ brands/ssil/update_shipping.py
@@ -0,0 +1,21 @@
+import utils
+
+
+def main():
+
+    client = utils.client("ssil")
+    import pprint
+
+    pprint.pprint(
+        client.update_delivery_flat_rate(
+            profile_name="General profile",
+            zone_name="国内配送",
+            new_price=880,
+            new_method_name="通常配送 お支払い完了から10-14日を目安にお届けします",
+            testrun=True,
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
