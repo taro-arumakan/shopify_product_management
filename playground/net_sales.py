e7fc5dbbd921991a7da80e69623734449de0b59d
taro <taro.arumakan@gmail.com>
2026-08-19 18:34:43 +0900

    add ASHEIS

--- playground/net_sales.py
+++ playground/net_sales.py
@@ -2,22 +2,23 @@ import datetime
 import utils
 
 brands = [
-    "GBH",
     "ROH",
-    "Archivepke",
     "KUME",
     "ApricotStudios",
     "BLOSSOM",
     "LEMEME",
     "SSIL",
     "A&ST",
+    "ASHEIS",
+    "GBH",
+    "Archivepke",
 ]
 
 net_sales_by_brand = {}
 for brand in brands:
     client = utils.client(brand)
     net_sales_by_brand[brand] = client.report_sales_kpi_by(
-        datetime.date(2026, 4, 1), datetime.date(2026, 4, 30)
+        datetime.date(2026, 7, 1), datetime.date(2026, 7, 31)
     ).iloc[0]["net_sales"]
 
 for b, n in net_sales_by_brand.items():
