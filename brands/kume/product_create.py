85c32c3140a6b7f668ffa678ce8deac269137f56
kentaro kimoto <exiler2411k@gmail.com>
2026-09-02 19:04:35 +0900

    CEC-484-kume-0916_0923-26fw-1st

--- brands/kume/product_create.py
+++ brands/kume/product_create.py
@@ -1,32 +1,55 @@
 import datetime
 import logging
 import zoneinfo
+
 from brands.kume.client import KumeClient
 
 logging.basicConfig(level=logging.INFO)
 
 
-def main():
-    sheet_name = "26FW_(1)09.09"
+def create_26fw_2_0916():
+    sheet_name = "26FW_(2)09.16"
     client = KumeClient(
-        product_sheet_start_row=2,
-        remove_existing_new_product_indicators=True,
-        products_season_tag="26_0909_FW_1",
+        product_sheet_start_row=1,
+        remove_existing_new_product_indicators=False,
+        products_season_tag="26_0916_FW_2",
     )
 
-    # filter_func = lambda product_input: product_input["release_date"] == "2026-04-06"
-
     scheduled_time = datetime.datetime(
-        2026, 9, 6, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+        2026, 9, 16, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
     )
 
     client.sanity_check_sheet(sheet_name)
-
     client.process_sheet_to_products(
         sheet_name,
         additional_tags=["New Arrival"],
         scheduled_time=scheduled_time,
     )
+
+
+def create_26fw_3_0923():
+    sheet_name = "26FW_(3)09.23"
+    client = KumeClient(
+        product_sheet_start_row=1,
+        remove_existing_new_product_indicators=False,
+        products_season_tag="26_0923_FW_3",
+    )
+
+    scheduled_time = datetime.datetime(
+        2026, 9, 23, 0, 0, 0, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")
+    )
+
+    client.sanity_check_sheet(sheet_name)
+    client.process_sheet_to_products(
+        sheet_name,
+        additional_tags=["New Arrival"],
+        scheduled_time=scheduled_time,
+    )
+
+
+def main():
+    create_26fw_2_0916()
+    create_26fw_3_0923()
 
 
 if __name__ == "__main__":
