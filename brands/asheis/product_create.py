94e5ff65d2f9889c0aa971e02261bad79154eebe
taro-arumakan <taro@sniarti.fi>
2026-09-24 11:20:14 +0900

    CEC-501 process Sep 24th

--- brands/asheis/product_create.py
+++ brands/asheis/product_create.py
@@ -12,28 +12,20 @@ def main():
         remove_existing_new_product_indicators=False,
         products_season_tag="26FW",
     )
-    sheet_names = [
-        # "【10デリ_1】Products Master",
-        "【10デリ_2】Products Master"
-    ]
-    additional_tags = [
-        # "26_oct_1",
-        "26_oct_2"
-    ]
+    sheet_names = ["【0924デリ】Products Master"]
+    additional_tags = ["26_sep"]
     scheduled_times = [
-        # datetime.datetime(2026, 9, 30, 12, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")),
-        datetime.datetime(2026, 10, 14, 12, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")),
+        datetime.datetime(2026, 9, 24, 12, tzinfo=zoneinfo.ZoneInfo("Asia/Tokyo")),
     ]
 
-    # for sheet_name in sheet_names:
-    #     client.sanity_check_sheet(sheet_name)
+    for sheet_name in sheet_names:
+        client.sanity_check_sheet(sheet_name)
 
     for sn, at, st in zip(sheet_names, additional_tags, scheduled_times):
         client.process_sheet_to_products(
             sn,
             additional_tags=[at],
             scheduled_time=st,
-            restart_at_product_title="WOOL BLEND CROPPED JACKET",
         )
 
 
